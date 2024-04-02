2400d2fa099db23aa9c93a0c2df4872e8663dbdd
dotuangv <117527123+dotuangv@users.noreply.github.com>
2024-04-02 08:42:11 +0700

    Time: 6 ms (65.28%) | Memory: 8 MB (94.60%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        vector<bool> check(s.size(), false);
-        map<char, vector<int>> m1, m2;
-        for(int i = 0; i < s.size(); i++)
-        {
-            m1[s[i]].push_back(i);
-            m2[t[i]].push_back(i);
-        }
-        for(int i = 0; i < s.size(); i++)
-        {
-            if(!check[i])
-            {
-                if(m1[s[i]].size() != m2[t[i]].size()) return false;
-                else
-                {
-                    for(int j = 0; j < m1[s[i]].size(); j++)
-                    {
-                        if(m1[s[i]][j] != m2[t[i]][j]) return false;
-                        check[m1[s[i]][j]] = true;
-                    }
-                }
-                check[i] = true;
+        int hash[256]= {0};
+        bool istCharmapped[256]= {0};
+        for(int i=0; i<s.size(); i++){
+            if(hash[s[i]]==0 && istCharmapped[t[i]] ==0){
+                hash[s[i]]= t[i];
+                istCharmapped[t[i]]= true; 
             }
         }
-        return true;
+
+        for(int i=0; i<s.size(); i++){
+            if (char(hash[s[i]])!= t[i] ){
+                return false; 
+            }
+        }
+        return true; 
     }
 };
