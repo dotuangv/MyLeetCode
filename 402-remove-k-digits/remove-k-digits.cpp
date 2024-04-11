d8d7deeb8fc4276f9eb68fb69f71b9ed052f8bc7
dotuangv <117527123+dotuangv@users.noreply.github.com>
2024-04-11 12:47:25 +0700

    Time: 8 ms (80.81%) | Memory: 9.7 MB (93.46%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.cpp
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -1,42 +1,17 @@
 class Solution {
 public:
-    string removeKdigits(string nums, int k) {
-        if(nums.size() == k) return "0";
-        string ans = "";
-        int zero = 0;
-        for(int i = 0; i < nums.size(); i++)
-        {
-            if(k != 0)
+    string removeKdigits(string num, int k) {
+        string s="";
+        for(char c:num){
+            while(s.size() && s.back()>c && k)
             {
-                while(k && ans.size() > 0 && nums[i] < ans.back())
-                {
-                    k--;
-                    ans.pop_back();
-                }
-                ans += nums[i];
-            }else
-            {
-                for(int j = i; j < nums.size(); j++)
-                {
-                    ans += nums[j];
-                }
-                break;
+                k--;
+                s.pop_back();
             }
+            if(s.size()||c!='0') s.push_back(c);
         }
-        if(k > 0)
-        {
-            while(k--) ans.pop_back();
-        }
-
-        string s = "";
-        int i = 0;
-        while(i < ans.size())
-        {
-            if(ans[i] != '0') break;
-            i++;
-        }
-        while(i < ans.size()) s += ans[i++];
-        if(s.size() == 0) s += '0';
-        return s;
+        while(s.size() && k--)
+            s.pop_back();
+        return (s.size()==0)?"0":s;
     }
 };
