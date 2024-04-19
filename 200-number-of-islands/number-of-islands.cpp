5213bfc88ba7ee7efdc2829f014c2a9056b0db7b
dotuangv <117527123+dotuangv@users.noreply.github.com>
2024-04-19 08:01:29 +0700

    Time: 19 ms (98.04%) | Memory: 15.6 MB (94.19%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -6,30 +6,28 @@ auto init = []() {
 }();
 class Solution {
 public:
-    vector<vector<bool>> visit;
     int x[4] = {-1, 0, 0, 1};
     int y[4] = {0, -1, 1, 0};
     void solve(int i, int j, vector<vector<char>> &grid)
     {
-        visit[i][j] = true;
+        grid[i][j] = '0';
         for(int l = 0; l < 4; l++)
         {
             int ii = i + x[l];
             int jj = j + y[l];
-            if(ii >= 0 && ii < grid.size() && jj >= 0 && jj < grid[i].size() && grid[ii][jj] == '1' && !visit[ii][jj])
+            if(ii >= 0 && ii < grid.size() && jj >= 0 && jj < grid[i].size() && grid[ii][jj] == '1')
             {
                 solve(ii, jj, grid);
             }
         }  
     }
     int numIslands(vector<vector<char>>& grid) {
-        visit.resize(grid.size(), vector<bool> (grid[0].size(), false));
         int ans = 0;
         for(int i = 0; i < grid.size(); i++)
         {
             for(int j = 0; j < grid[i].size(); j++)
             {
-                if(!visit[i][j] && grid[i][j] == '1')
+                if(grid[i][j] == '1')
                 {
                     ans++;
                     solve(i, j, grid);
