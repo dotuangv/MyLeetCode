3acade3ba2f0179aca7afae9322198eab91bf2f7
dotuangv <117527123+dotuangv@users.noreply.github.com>
2024-04-26 06:45:39 +0700

    Time: 0 ms (100.00%) | Memory: 9.6 MB (98.77%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,38 +1,52 @@
 class Solution {
 public:
+
     bool isMatch(string s, string p) {
-        if(s.size() == 0)
-        {
-            for(int i = 0; i < p.size(); i++) if(p[i] != '*') return false;
-            return true;
-        }
-        vector<vector<bool>> dp(p.size() + 1, vector<bool> (s.size() + 1));
-        dp[0][0] = true;
-        int cnt = 0;
-        for(int i = 1; i <= p.size(); i++)
-        {
-            bool ok = false;
-            for(int j = 1; j <= s.size(); j++)
-            {
-                if(p[i - 1] == '*')
-                {
-                    dp[i][j] = max({dp[i - 1][j - 1], dp[i][j - 1], dp[i - 1][j]});
-                    if(cnt == 0) dp[i][0] = true;
-                }
-                else if(p[i - 1] == '?')
-                {
-                    dp[i][j] = dp[i - 1][j - 1];
-                    cnt++;
-                }
-                else if(p[i - 1] == s[j - 1])
-                {
-                    dp[i][j] = dp[i - 1][j - 1];
-                    cnt++;
-                }
-                if(dp[i][j]) ok = true;
-            }
-            if(!ok) return false;
-        }
-        return dp[p.size()][s.size()];
-    }
+
+	int n = s.length();
+	int m = p.length();
+	int i = 0, j = 0, startIndex = -1, match = 0;
+
+	while (i < n)
+	{
+		if (j < m && (p[j] == '?' || p[j] == s[i]))
+		{
+			// Characters match or '?' in pattern matches any character.
+			i++;
+			j++;
+		}
+		else if (j < m && p[j] == '*')
+		{
+			// Wildcard character '*', mark the current position in the pattern and the text as a proper match.
+			startIndex = j;
+			match = i;
+			j++;
+		}
+		else if (startIndex != -1)
+		{
+			// No match, but a previous wildcard was found. Backtrack to the last '*' character position and try for a different match.
+			j = startIndex + 1;
+			match++;
+			i = match;
+		}
+		else
+		{
+			// If none of the above cases comply, the pattern does not match.
+			return false;
+		}
+	}
+
+	// Consume any remaining '*' characters in the given pattern.
+	while (j < m && p[j] == '*')
+	{
+		j++;
+	}
+
+	// If we have reached the end of both the pattern and the text, the pattern matches the text.
+	return j == m;
+}
+
+
+   
+    
 };
