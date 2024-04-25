348784aee8fe49b77ca1f4390a121f5167a33176
dotuangv <117527123+dotuangv@users.noreply.github.com>
2024-04-25 17:51:57 +0700

    Time: 39 ms (85.29%) | Memory: 11.5 MB (84.45%) - LeetSync

--- 2444-longest-ideal-subsequence/longest-ideal-subsequence.cpp
+++ 2444-longest-ideal-subsequence/longest-ideal-subsequence.cpp
@@ -2,19 +2,20 @@ static auto _ = []() { std::ios_base::sync_with_stdio(false); std::cin.tie(nullp
 class Solution {
 public:
     int longestIdealString(string s, int k) {
-        vector<int> dp(s.size(), 1), mp(26, -1);
+        vector<int> dp(26);
         int ans = 0;
         for(int i = 0; i < s.size(); i++)
         {
+            int Max = 0;
             for(int j = 0; j < 26; j++)
             {
                 if(abs(s[i] - 'a' - j) <= k)
                 {
-                    if(mp[j] != -1) dp[i] = max(dp[i], dp[mp[j]] + 1);
+                    Max = max(Max, dp[j] + 1);
                 }
             }
-            mp[s[i] -'a'] = i;
-            ans = max(ans, dp[i]);
+            dp[s[i] - 'a'] = max(Max, dp[s[i] - 'a']);
+            ans = max(ans, dp[s[i] - 'a']);
         }
         return ans;
     }
