eadb4e977f1027210e39ad5646b22d227342a8c3
dotuangv <117527123+dotuangv@users.noreply.github.com>
2024-04-26 16:33:20 +0700

    Time: 0 ms (100.00%) | Memory: 8 MB (93.64%) - LeetSync

--- 43-multiply-strings/multiply-strings.cpp
+++ 43-multiply-strings/multiply-strings.cpp
@@ -8,12 +8,9 @@ public:
         reverse(num2.begin(), num2.end());
         for(int i = 0; i < num1.size(); i++)
         {
-            int a = num1[i] - 48;
             for(int j = 0; j < num2.size(); j++)
             {
-                int b = num2[j] - 48;
-                int c = ans[i + j] - 48;
-                int s = a*b + c + tmp;
+                int s = (num2[j] - 48)*(num1[i] - 48) + (ans[i + j] - 48) + tmp;
                 ans[i + j] = char(s % 10 + 48);
                 tmp = s / 10; 
             }
