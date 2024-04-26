57b805eedae3ac42f04093e875d3e0769836f74c
dotuangv <117527123+dotuangv@users.noreply.github.com>
2024-04-26 08:20:58 +0700

    Time: 156 ms (59.84%) | Memory: 19.5 MB (8.77%) - LeetSync

--- 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
@@ -1,22 +1,69 @@
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& grid) {
-        vector<vector<int>> dp(grid.size(), vector<int> (grid[0].size()));
-        for(int i = 0; i < grid[0].size(); i++) dp[0][i] = grid[0][i];
+        vector<vector<int>> Min(grid.size(), vector<int> (grid[0].size()));
+        if(grid.size() == 1) return grid[0][0];
+        int Min1, Min2;
+        if(grid[0][0] < grid[0][1])
+        {
+            Min1 = 0;
+            Min2 = 1;
+        }else
+        {
+            Min1 = 1;
+            Min2 = 0;
+        }
+        for(int j = 2; j < grid[0].size(); j++)
+        {
+            if(grid[0][j] < grid[0][Min1])
+            {
+                if(Min1 != j) Min2 = Min1;
+                Min1 = j;
+            }else if(grid[0][j] < grid[0][Min2] && j != Min1) Min2 = j;
+        }
+        for(int j = 0; j < grid[0].size(); j++)
+        {
+            if(j != Min1) Min[0][j] = grid[0][Min1];
+            else Min[0][j] = grid[0][Min2];
+        }
+        // for(int i = 0; i < grid.size(); i++)
+        // {
+        //     for(int j = 0; j < grid.size(); j++) cout << Min[i][j] << " ";
+        //     cout << "\n";
+        // }
         for(int i = 1; i < grid.size(); i++)
         {
+            vector<int> dp(grid.size());
             for(int j = 0; j < grid[i].size(); j++)
             {
-                int Min = 1e9;
-                for(int l = 0; l < grid[i].size(); l++)
+                dp[j] = grid[i][j] + Min[i - 1][j];
+            }
+            int Min1, Min2;
+            if(dp[0] < dp[1])
+            {
+                Min1 = 0;
+                Min2 = 1;
+            }else
+            {
+                Min1 = 1;
+                Min2 = 0;
+            }
+            for(int j = 0; j < grid.size(); j++)
+            {
+                if(dp[j] < dp[Min1])
                 {
-                    if(l != j) Min = min(Min, dp[i - 1][l]);
+                    if(Min1 != j) Min2 = Min1;
+                    Min1 = j;
+                }else if(dp[j] < dp[Min2] && j != Min1) Min2 = j;
+                for(int j = 0; j < grid.size(); j++)
+                {
+                    if(j != Min1) Min[i][j] = dp[Min1];
+                    else Min[i][j] = dp[Min2];
                 }
-                dp[i][j] = grid[i][j] + Min;
             }
         }
         int ans = 1e9;
-        for(int i = 0; i < grid[0].size(); i++) ans = min(ans, dp[grid.size() - 1][i]);
+        for(int i = 0; i < grid.size(); i++) ans = min(ans, Min[grid.size() - 1][i]);
         return ans;
     }
 };
