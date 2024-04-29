bd437b592b1b4e500940801892cd08ffca007916
dotuangv <117527123+dotuangv@users.noreply.github.com>
2024-04-29 20:25:12 +0700

    Time: 48 ms (99.65%) | Memory: 91.9 MB (6.81%) - LeetSync

--- 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -4,33 +4,17 @@ public:
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
         cout.tie(NULL);
-        vector<vector<int>> bit(nums.size(), vector<int> (20));
-        for(int i = 0; i < nums.size(); i++)
-        {
-            int j = 0;
-            while(nums[i])
-            {
-                bit[i][j++] = nums[i] % 2;
-                nums[i] /= 2;
-            }
+        int ans=0;
+        for(auto x:nums){
+            ans=ans^x;
         }
-        vector<int> bitk(20);
-        int j = 0;
-        while(k)
-        {
-            bitk[j++] = k % 2;
-            k /= 2;
+        ans=ans^k;
+        int res=0;
+        while(ans>0){
+            if(ans&1)
+            res++;
+            ans=ans>>1;
         }
-        int ans = 0;
-        for(int i = 0; i < 20; i++)
-        {
-            int t = 0;
-            for(int j = 0; j < nums.size(); j++) t ^= bit[j][i];
-            if(t != bitk[i])
-            {
-                ans++;
-            }
-        }
-        return ans;
+        return res;
     }
 };
