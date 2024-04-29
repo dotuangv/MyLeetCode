46997f73e89539c13eddfb94f113abd2b032477b
dotuangv <117527123+dotuangv@users.noreply.github.com>
2024-04-29 20:24:02 +0700

    Time: 300 ms (5.06%) | Memory: 204.8 MB (6.81%) - LeetSync

---
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        vector<vector<int>> bit(nums.size(), vector<int> (20));
+        for(int i = 0; i < nums.size(); i++)
+        {
+            int j = 0;
+            while(nums[i])
+            {
+                bit[i][j++] = nums[i] % 2;
+                nums[i] /= 2;
+            }
+        }
+        vector<int> bitk(20);
+        int j = 0;
+        while(k)
+        {
+            bitk[j++] = k % 2;
+            k /= 2;
+        }
+        int ans = 0;
+        for(int i = 0; i < 20; i++)
+        {
+            int t = 0;
+            for(int j = 0; j < nums.size(); j++) t ^= bit[j][i];
+            if(t != bitk[i])
+            {
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
