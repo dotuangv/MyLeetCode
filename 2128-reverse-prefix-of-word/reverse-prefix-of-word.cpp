1749d308c1b8f69ff55c482d72ee7718cbe75b3e
dotuangv <117527123+dotuangv@users.noreply.github.com>
2024-05-01 11:48:58 +0700

    Time: 1 ms (43.54%) | Memory: 7.4 MB (91.39%) - LeetSync

---
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int i = 0;
+        while(i < word.size() && word[i] != ch) i++;
+        if(i < word.size())
+        {
+            int j = 0;
+            while(j <= i)
+            {
+                swap(word[i], word[j]);
+                i--;
+                j++;
+            }
+        }
+        return word;
+    }
+};
