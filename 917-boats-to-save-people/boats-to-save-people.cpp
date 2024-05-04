8f80c1ddb8d4fbf36739a198cef998de575ed2a0
dotuangv <117527123+dotuangv@users.noreply.github.com>
2024-05-04 11:44:35 +0700

    Time: 29 ms (99.85%) | Memory: 45.5 MB (46.86%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.cpp
+++ 917-boats-to-save-people/boats-to-save-people.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
+        std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
         int ans = 0;
         int l = 0, r = people.size() - 1;
         sort(people.begin(), people.end());
