808ea58df3eea22bd0ff8bafe25bddafdc24bae5
dotuangv <117527123+dotuangv@users.noreply.github.com>
2024-05-17 08:23:48 +0700

    Time: 7 ms (92.90%) | Memory: 21.4 MB (9.62%) - LeetSync

--- 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.cpp
+++ 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.cpp
@@ -11,55 +11,23 @@
  */
 class Solution {
 public:
-    void dfs(TreeNode *root, int target, bool &check)
-    {
-        if(root != NULL)
-        {
-            if(root->left != NULL && root->right != NULL)
-            {
-                dfs(root->left, target, check);
-                if(check)
-                {
-                    root->left = NULL;
-                    check = false;
-                }
-                dfs(root->right, target, check);
-                if(check)
-                {
-                    root->right = NULL;
-                    check = false;
-                }
-                if(root->left == NULL && root->right == NULL && root->val == target) check = true;
-            }
-            else if(root->left != NULL)
-            {
-                dfs(root->left, target, check);
-                if(check)
-                {
-                    root->left = NULL;
-                    check = false;
-                }
-                if(root->left == NULL && root->right == NULL && root->val == target) check = true;
-            }else if(root->right != NULL)
-            {
-                dfs(root->right, target, check);
-                if(check)
-                {
-                    root->right = NULL;
-                    check = false;
-                }
-                if(root->left == NULL && root->right == NULL && root->val == target) check = true;
-            }else
-            {
-                if(root->val == target) check = true;
-            }
-        }
-    }
-
     TreeNode* removeLeafNodes(TreeNode* root, int target) {
-        bool check = false;
-        dfs(root, target, check);
-        if(root->left == NULL && root->right == NULL && root->val == target) return NULL;
+        if(!root)
+            return NULL;
+
+        if(!root->left && !root->right){
+            if(root->val == target)
+                return NULL;
+            else
+                root;
+        }
+
+        root->left = removeLeafNodes(root->left, target);
+        root->right = removeLeafNodes(root->right, target);
+
+        if(root->val == target && !root->left && !root->right)
+            return NULL;
+
         return root;
     }
 };
