25e947063c3285e1deaa991ecd8b433308f4be33
dotuangv <117527123+dotuangv@users.noreply.github.com>
2024-05-22 23:48:50 +0700

    Time: 62 ms (97.16%) | Memory: 52.6 MB (93.78%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,39 +1,28 @@
 class Solution {
 public:
     vector<vector<string>> partition(string s) {
-        vector<vector<string>> result;
-        vector<string> path;
-        backtrack(s, 0, path, result);
-        return result;
+        vector<vector<string>> ans;
+        vector<string> tmp;
+        dfs(0, tmp, s, ans);
+        return ans;
     }
 
-private:
-    void backtrack(const string& s, int start, vector<string>& path, vector<vector<string>>& result) {
-        // If we've reached the end of the string, add the current partition to the result list
-        if (start == s.length()) {
-            result.push_back(path);
-            return;
-        }
-        // Explore all possible partitions
-        for (int end = start + 1; end <= s.length(); ++end) {
-            // If the current substring is a palindrome, add it to the current path
-            if (isPalindrome(s, start, end - 1)) {
-                path.push_back(s.substr(start, end - start));
-                // Recur to find other partitions
-                backtrack(s, end, path, result);
-                // Backtrack to explore other partitions
-                path.pop_back();
-            }
-        }
-    }
-
-    bool isPalindrome(const string& s, int left, int right) {
-        // Check if the substring s[left:right+1] is a palindrome
-        while (left < right) {
-            if (s[left++] != s[right--]) {
-                return false;
-            }
+    bool isPalindrome(const string& str) {
+        for(int i = 0; i < (str.size() / 2); i++) {
+            if (str[i] != str[str.size() - 1 - i]) return false;
         }
         return true;
+    }
+
+    void dfs(int i, vector<string>& cur, const string& s, vector<vector<string>>& ans) {
+        if (i == s.size()) {
+            ans.push_back(cur);
+        }
+        for(int start = i; i < s.size(); i++) {
+            cur.push_back(s.substr(start, i - start + 1));
+            if (isPalindrome(cur.back()))
+                dfs(i + 1, cur, s, ans);
+            cur.pop_back();
+        }
     }
 };
