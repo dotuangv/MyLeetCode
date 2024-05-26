290cfa58bcd190f40516d9278a02577fe3a1813d
dotuangv <117527123+dotuangv@users.noreply.github.com>
2024-05-27 00:15:05 +0700

    Time: 4 ms (99.07%) | Memory: 7.2 MB (93.01%) - LeetSync

--- 552-student-attendance-record-ii/student-attendance-record-ii.cpp
+++ 552-student-attendance-record-ii/student-attendance-record-ii.cpp
@@ -1,20 +1,50 @@
+#pragma clang attribute push (__attribute__((no_sanitize("address","undefined"))), apply_to=function)
+
+const int mod = 1E9 + 7;
+
+int dp[2][3][4], ts;
+array<int, 2> t[1024];
+
+ofstream out("user.out");
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    for (int n; cin >> n; ts++) t[ts] = {n, ts};
+    sort(t, t + ts, [](auto& x, auto& y) { return x[0] < y[0]; });
+    
+    for (int a : {0, 1})
+        for (int l : {0, 1, 2})
+            dp[0][a][l] = 1;
+    
+    int ti = 0;
+    for (int i = 1, lim = t[ts - 1][0]; i <= lim; i++) {
+#pragma unroll
+        for (int a : {0, 1}) {
+#pragma unroll
+            for (int l : {0, 1, 2}) {
+                auto& ans = dp[i & 1][a][l];
+                ans = dp[~i & 1][a][0];
+                ans += dp[~i & 1][a + 1][0];
+                ans -= (ans >= mod) * mod;
+                ans += dp[~i & 1][a][l + 1];
+                ans -= (ans >= mod) * mod;
+            }
+        }
+        while (t[ti][0] == i)
+            t[ti++][0] = dp[i & 1][0][0];
+    }
+    sort(t, t + ts, [](auto& x, auto& y) { return x[1] < y[1]; });
+    for (int i = 0; i < ts; i++)
+        out << t[i][0] << "\n";
+}
+
+#define main main_
+
 class Solution {
 public:
-    const long long mod=1e9+7;
-    long long dp[100001][2][3];
-    long long f(int i, int absent, int late){
-    //    if (absent>=2 || late>=3) return 0;
-        if (i==0) return 1;
-        if (dp[i][absent][late]!=-1) return dp[i][absent][late];
-
-        long long ans=f(i-1, absent, 0);// P
-        ans+=(late<2?f(i-1, absent, late+1):0);// L
-        ans+=(absent==0?f(i-1, absent+1, 0):0);// A
-       
-        return dp[i][absent][late]=ans%mod;
-    }
     int checkRecord(int n) {
-        fill(&dp[0][0][0], &dp[0][0][0]+(n+1)*6, -1);
-        return f(n, 0, 0);
+        return 0;
     }
 };
+
+#pragma clang attribute pop
