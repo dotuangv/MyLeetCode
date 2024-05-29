d6450945fa59f19153370123d2601cf34f657243
dotuangv <117527123+dotuangv@users.noreply.github.com>
2024-05-29 14:54:56 +0700

    Time: 4 ms (25.58%) | Memory: 7.7 MB (25.06%) - LeetSync

--- 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -1,40 +1,24 @@
 class Solution {
 public:
     int numSteps(string s) {
-        int ans = 0;
-        reverse(s.begin(), s.end());
-        while(1)
-        {
-            if(s.size() == 1) return ans;
-            if(s[0] == '1')
-            {
-                int tmp = 1;
-                s[0] = '0';
-                for(int i = 1; i < s.size(); i++)
-                {
-                    if(s[i] == '1') s[i] = '0';
-                    else
-                    {
-                        s[i] = '1';
-                        tmp = 0;
-                        break;
-                    }
-                }
-                if(tmp) s.push_back('1');
-            }else
-            {
-                for(int i = 0; i < s.size() - 1; i++)
-                {
-                    if(s[i] == '0' && s[i + 1] == '1')
-                    {
-                        s[i] = '1';
-                        s[i + 1] = '0';
-                    }
-                }
+        int sol = 0;
+        while (s != "1") {
+            if (s.back() == '0') {
                 s.pop_back();
+            } else {
+                int i = s.size()-1;
+                while (i >= 0 && s[i] == '1') {
+                    s[i] = '0';
+                    i--;
+                }
+                if (i < 0) {
+                    s = '1' + s;
+                } else {
+                    s[i] = '1';
+                }
             }
-            ans++;
+            sol++;
         }
-        return ans;
+        return sol;
     }
 };
