cb5377ab64a4016d94f9813e4227c8be1dbd5f5f
dotuangv <117527123+dotuangv@users.noreply.github.com>
2024-06-06 10:20:30 +0700

    Time: 64 ms (43.80%) | Memory: 31.7 MB (29.12%) - LeetSync

---
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        if(hand.size() % groupSize) return false;
+        sort(hand.begin(), hand.end());
+        map<int, int> mp;
+        for(auto x: hand) mp[x]++;
+        for(auto &x: mp)
+        {
+            // cout << x.first << " " << x.second << "\n";
+            if(x.second > 0)
+            {
+                for(int i = x.first + 1; i < x.first + groupSize; i++)
+                {
+                    if(mp[i] < x.second) return false;
+                    mp[i] -= x.second;
+                }
+            }else if(x.second < 0) return false;
+        }
+        return true;
+    }
+};
