a2f70e01f435148224631841202da92fee90b2bf
dotuangv <117527123+dotuangv@users.noreply.github.com>
2024-06-13 13:48:37 +0700

    Time: 0 ms (100.00%) | Memory: 21.1 MB (17.35%) - LeetSync

--- 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.cpp
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.cpp
@@ -1,27 +1,12 @@
 class Solution {
 public:
     int minMovesToSeat(vector<int>& seats, vector<int>& students) {
-        vector<int> vt1(101), vt2(101);
-        for(auto &x: seats) vt1[x]++;
-        for(auto &x: students) vt2[x]++;
-        int i = 1, j = 1, ans = 0;
-        while(1)
-        {
-            while(i <= 100 && vt1[i] == 0) i++;
-            while(j <= 100 && vt2[j] == 0) j++;
-            if(i > 100 && j > 100) return ans;
-            if(vt1[i] >= vt2[j])
-            {
-                ans += vt2[j]*abs(i - j);
-                vt1[i] -= vt2[j];
-                vt2[j] = 0;
-            }else
-            {
-                ans += vt1[i]*abs(i - j);
-                vt2[j] -= vt1[i];
-                vt1[i] = 0;
-            }
-        }
-        return ans;
+       sort(seats.begin(), seats.end());
+       sort(students.begin(), students.end());
+        int moves = 0;
+        for( int i=0; i<seats.size(); i++)
+            moves += abs(seats[i] - students[i]);
+        
+        return moves;
     }
 };
