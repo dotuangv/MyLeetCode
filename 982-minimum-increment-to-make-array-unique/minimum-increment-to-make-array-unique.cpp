64216c4304c4b14899b943771736c749023b7acb
dotuangv <117527123+dotuangv@users.noreply.github.com>
2024-06-15 00:00:42 +0700

    Time: 82 ms (97.24%) | Memory: 68.8 MB (96.85%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
@@ -1,15 +1,22 @@
-
 class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int ans = 0;
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]<=nums[i-1]){
-                ans += nums[i-1]-nums[i]+1;
-                nums[i]= nums[i-1]+1;
-            }
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+     int count =0;
+     sort(nums.begin(),nums.end());
+     for(int i=1;i<nums.size();i++){
+        if(nums[i]==nums[i-1]){
+            int diff=1;
+            count+=diff;
+            nums[i]=nums[i]+diff;
         }
-        return ans;
+        if(nums[i]<nums[i-1]){
+            int diff=nums[i-1]-nums[i]+1;
+            count+=diff;
+            nums[i]=nums[i]+diff;
         }
+     }
+     return count;
+    }
 };
