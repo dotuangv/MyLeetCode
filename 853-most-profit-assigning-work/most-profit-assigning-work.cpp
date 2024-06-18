bf49899631980c3968dca805daa874c5e7e7a2f7
dotuangv <117527123+dotuangv@users.noreply.github.com>
2024-06-18 11:57:25 +0700

    Time: 78 ms (28.99%) | Memory: 48.4 MB (16.24%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.cpp
+++ 853-most-profit-assigning-work/most-profit-assigning-work.cpp
@@ -1,62 +1,21 @@
 class Solution {
 public:
-Solution(){
-ios::sync_with_stdio(0);
-cout.tie(0);
-cin.tie(0);
-}
-    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
-        /*
-        int diffmax = INT_MIN,workermax = INT_MIN;
-        for(int i : difficulty)
-        if(i> diffmax)
-        diffmax = i;
-        for(int i: worker)
-        if(i>workermax)
-        workermax = i;
-
-       int countMax = diffmax > workermax? diffmax:workermax;
-       vector<int>  countArr(countMax+1); 
-       for(int i = 0; i < difficulty.size(); ++i)
-       {
-        if(profit[i] > countArr[difficulty[i]])
-       countArr[difficulty[i]] = profit[i]; 
-       }
-       for(int i =0;i<countArr.size()-1;++i)
-       {
-        if(countArr[i+1]==0)
-       countArr[i+1] = countArr[i]; 
-       else if(countArr[i+1]<countArr[i])
-       countArr[i+1] = countArr[i];
-       }
-       int sum = 0;
-       for(int i = 0; i<worker.size();++i)
-       {
-        sum += countArr[worker[i]] ;
-       }
-return sum;
-*/
-struct{
-bool operator() (const pair<int,int> first, const pair<int,int> second)  const
-{
-   return first.first < second.first; 
-}
-}cmp;
-vector <pair<int,int>> jobs;
-for(int i = 0; i<difficulty.size();++i)
-jobs.push_back({difficulty[i],profit[i]});;
-ranges::sort(jobs,cmp);
-ranges::sort(worker);
-int best = 0;
-int sum = 0;
-int i = 0;
-for(int w: worker)
-{
-    while(i < jobs.size() && jobs[i].first <= w)
-    best = max(best,jobs[i++].second);
-    sum += best;
-
-}
-return sum;
+    int maxProfitAssignment(vector<int>& dif, vector<int>& pro, vector<int>& worker) {
+        int MAX = worker[0];
+        for(auto &x: worker) MAX = max(MAX, x);
+        vector<int> Max(MAX + 1);
+        for(int i = 0; i < dif.size(); i++)
+        {
+            if(dif[i] > MAX) continue;
+            Max[dif[i]] = max(Max[dif[i]], pro[i]);
+        }
+        
+        for(int i = 1; i <= MAX; i++)
+        {
+            Max[i]  = max(Max[i - 1], Max[i]);
+        }
+        int ans =  0;
+        for(auto &x: worker) ans += Max[x];
+        return ans;
     }
 };
