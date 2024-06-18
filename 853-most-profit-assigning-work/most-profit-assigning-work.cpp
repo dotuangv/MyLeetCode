7ba1f794d4ad270df55ed3facb832570e9d507cd
dotuangv <117527123+dotuangv@users.noreply.github.com>
2024-06-18 11:16:45 +0700

    Time: 531 ms (16.36%) | Memory: 228.1 MB (5.03%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.cpp
+++ 853-most-profit-assigning-work/most-profit-assigning-work.cpp
@@ -2,12 +2,14 @@ class Solution {
 public:
     int maxProfitAssignment(vector<int>& dif, vector<int>& pro, vector<int>& worker) {
         unordered_map<int, int> mp;
+        int MAX = worker[0];
+        for(auto &x: worker) MAX = max(MAX, x);
         for(int i = 0; i < dif.size(); i++)
         {
             mp[dif[i]] = max(mp[dif[i]], pro[i]);
         }
-        vector<int> Max(100001);
-        for(int i = 1; i <= 100000; i++)
+        vector<int> Max(MAX + 1);
+        for(int i = 1; i <= MAX; i++)
         {
             Max[i]  = max(Max[i - 1], mp[i]);
         }
