c96bbd92f6f5313840dc9cba1e8b899ac6bf4dde
dotuangv <117527123+dotuangv@users.noreply.github.com>
2024-06-27 10:26:28 +0700

    Time: 86 ms (99.37%) | Memory: 70.8 MB (77.19%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0) ;
         unordered_map<int, int> mp;
         for(auto x: edges)
         {
