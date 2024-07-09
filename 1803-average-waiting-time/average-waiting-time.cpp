cd639dff81952c2633121cc0f0eb6c1c43d10a35
dotuangv <117527123+dotuangv@users.noreply.github.com>
2024-07-09 10:49:54 +0700

    Time: 152 ms (52.35%) | Memory: 77.5 MB (20.88%) - LeetSync

---
+++ 1803-average-waiting-time/average-waiting-time.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    double averageWaitingTime(vector<vector<int>>& customers) {
+        vector<int> dp(customers.size());
+        dp[0] = customers[0][0] + customers[0][1];
+        double sum = customers[0][1];
+        for(int i = 1; i < customers.size(); i++)
+        {
+            if(customers[i][0] >= dp[i - 1]) dp[i] = customers[i][0] + customers[i][1];
+            else dp[i] = dp[i - 1] + customers[i][1];
+            sum += dp[i] - customers[i][0];
+        }
+        return sum/customers.size();
+    }
+};
