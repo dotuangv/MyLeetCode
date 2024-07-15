ddb1fc2e32dbb210aef30da93d9c243e30082445
dotuangv <117527123+dotuangv@users.noreply.github.com>
2024-07-15 22:35:39 +0700

    Time: 955 ms (8.52%) | Memory: 318.5 MB (10.26%) - LeetSync

---
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* solve(int x, map<int, pair<int, int>> &mp)
+    {
+        TreeNode *t = new TreeNode(x);
+        if(mp[x].first == 0 && mp[x].second == 0) return t;
+        else
+        {
+            if(mp[x].first != 0)
+            {
+                t->left = solve(mp[x].first, mp);
+            }
+            if(mp[x].second != 0)
+            {
+                t->right = solve(mp[x].second, mp);
+            }
+            return t;
+        }
+    }
+
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+        map<int, pair<int, int>> mp;
+        map<int, bool> mp2;
+        for(auto &x: descriptions)
+        {
+            if(x[2]) mp[x[0]].first = x[1];
+            else mp[x[0]].second = x[1];
+            mp2[x[1]] = true;
+        }
+        int head;
+        for(auto &x: mp)
+        {
+            if(!mp2[x.first])
+            {
+                head = x.first;
+                break;
+            }
+        }
+        return solve(head, mp);
+    }
+};
