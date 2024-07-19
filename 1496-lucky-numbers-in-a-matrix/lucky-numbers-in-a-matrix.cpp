4d690c3a7f88f77b7795669995080d9546ce4e48
dotuangv <117527123+dotuangv@users.noreply.github.com>
2024-07-19 21:06:25 +0700

    Time: 15 ms (64.89%) | Memory: 14.5 MB (12.50%) - LeetSync

--- 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.cpp
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.cpp
@@ -1,13 +1,9 @@
-auto init = []()
-{ 
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 'c';
-}();
 class Solution {
 public:
     vector<int> luckyNumbers (vector<vector<int>>& matrix) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         vector<int> maxCol(matrix[0].size());
         vector<int> minRow(matrix.size(), 1e9);
         vector<int> ans;
