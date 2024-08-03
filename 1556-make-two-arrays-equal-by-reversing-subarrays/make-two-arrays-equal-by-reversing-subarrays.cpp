e6d8dcc6e6bf6dcad6f86f300e30cb3fe42ddf2f
dotuangv <117527123+dotuangv@users.noreply.github.com>
2024-08-04 00:59:46 +0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (33.93%) - LeetSync

--- 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -1,14 +1,29 @@
+// Start Of Anuj Bhati aka Edwards310's Template...
+#pragma GCC optimize("O2")
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+
+#define FOR(a, b, c) for (int a = b; a < c; a++)
+#define FOR1(a, b, c) for (int a = b; a <= c; ++a)
+#define Rep(i, n) FOR(i, 0, n)
+#define Rep1(i, n) FOR1(i, 1, n)
+#define fi first
+
+// end of Anuj Bhati's aka Edwards310's Template
 class Solution {
 public:
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
-        vector<int> cnt1(1001);
-        vector<int> cnt2(1001);
-        for (int& v : target) {
-            ++cnt1[v];
-        }
-        for (int& v : arr) {
-            ++cnt2[v];
-        }
-        return cnt1 == cnt2;
+        vi help(1001, 0);
+        Rep(i, target.size())
+            help[target[i]]++;
+        
+        Rep(i, arr.size())
+            help[arr[i]]--;
+        
+        Rep(i, 1001)
+            if(help[i])
+                return false;
+        return true;
     }
 };
