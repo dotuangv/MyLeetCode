952aeeb62d4f32100347f2372099f0da055e2a65
dotuangv <117527123+dotuangv@users.noreply.github.com>
2024-08-05 16:46:55 +0700

    Time: 21 ms (30.77%) | Memory: 18.4 MB (31.70%) - LeetSync

---
+++ 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string kthDistinct(vector<string>& arr, int k) {
+        map<string, int> mp;
+        for(auto &x: arr) mp[x]++;
+        int res = 1;
+        for(auto &x: arr)
+        {
+            if(mp[x] == 1)
+            {
+                if(res == k) return x;
+                else res++;
+            }
+        }
+        return "";
+    }
+};
