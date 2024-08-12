9b970fba2272997905b9ca9f0a656ba0b3a75ae5
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2024-08-12 23:41:28 +0700

    Time: 31 ms (22.46%) | Memory: 24.9 MB (17.04%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.cpp
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.cpp
@@ -1,29 +1,24 @@
 class KthLargest {
 public:
-    KthLargest(int k, vector<int>& nums) : k(k) {
-        for (int num : nums) {
-            if (minHeap.size() < k) {
-                minHeap.push(num);
-            } else if (num > minHeap.top()) {
-                minHeap.push(num);
-                if (minHeap.size() > k) {
-                    minHeap.pop();
-                }
-            }
+    priority_queue<int,vector<int>,greater<int>> pq;
+    int size;
+    KthLargest(int k, vector<int>& nums) {
+        size=k;
+        for(int i=0;i<nums.size();i++){
+            pq.push(nums[i]);
+            if(pq.size()>k) pq.pop();
         }
     }
-
+    
     int add(int val) {
-        if (minHeap.size() < k) {
-            minHeap.push(val);
-        } else if (val > minHeap.top()) {
-            minHeap.push(val);
-            minHeap.pop();
-        }
-        return minHeap.top();
+        pq.push(val);
+        if(pq.size()>size) pq.pop();
+        return pq.top();
     }
-
-private:
-    int k;
-    priority_queue<int, vector<int>, greater<int>> minHeap;  // Min-heap to store the k largest elements
 };
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
