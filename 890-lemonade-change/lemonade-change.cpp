bf485f57c2bf216b1c0abdf2cb9474b7e10eaafb
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2024-08-16 00:15:03 +0700

    Time: 80 ms (22.48%) | Memory: 87.3 MB (5.06%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        unordered_map<int, int> mp;
+        for(auto &x: bills)
+        {
+            if(x == 5) mp[5]++;
+            else if(x == 10)
+            {
+                if(!mp[5]) return false;
+                mp[5]--;
+                mp[10]++;
+            }else
+            {
+                int x = 15;
+                if(mp[10])
+                {
+                    x -= 10;
+                    mp[10]--;
+                }
+                if(mp[5] < x/5) return false;
+                mp[5] -= x/5;
+            }
+        }
+        return true;
+    }
+};
