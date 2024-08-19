3b6e2ffa914f08b93197e79d75996a962a41ec8c
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2024-08-19 10:52:06 +0700

    Time: 7 ms (49.95%) | Memory: 8.2 MB (38.21%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
     int minSteps(int n) {
-        if(n == 1) return 0;
         vector<int> dp(n + 1);
-        dp[1] = 0;
-        dp[2] = 2;
-        for(int i = 3; i <= n; i++)
+        for(int i = 2; i <= n; i++)
         {
             dp[i] = i;
             for(int j = 2; j <= sqrt(i); j++)
