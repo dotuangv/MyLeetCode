13b43f7edd1d0002a2b7bc4d7f77a418610473bc
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2024-09-14 10:56:56 +0700

    Time: 45 ms (97.81%) | Memory: 85.6 MB (6.58%) - LeetSync

--- 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.cpp
+++ 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
         int Max = *max_element(nums.begin(), nums.end());
         int ans = 1, l = 0, r = 0;
         while(r < nums.size())
