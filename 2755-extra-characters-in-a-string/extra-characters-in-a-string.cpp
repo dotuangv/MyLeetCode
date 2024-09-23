3a91891e6637d20f9b794a7ee6ed610dd0072ac6
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2024-09-23 21:34:24 +0700

    Time: 55 ms (94.43%) | Memory: 61.2 MB (95.44%) - LeetSync

---
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int trie[3000][26];
+    bool ok[3000];
+    int cnt = 0;
+    void insert(string &word) {
+        int u = 0;
+        for(int i = 0; i < word.size(); i++)
+        {
+            int k = word[i] - 'a';
+            if(!trie[u][k]) trie[u][k] = ++cnt;
+            u = trie[u][k];
+        }
+        ok[u] = true;
+    }
+
+    void search(string &word, int j, vector<int> &dp, int t) {
+        int u = 0;
+        for(int i = j; i < word.size(); i++)
+        {
+            int k = word[i] - 'a';
+            if(!trie[u][k]) return;
+            u = trie[u][k];
+            if(ok[u])
+            {
+                dp[t] = max(dp[t], i - j + 1 + dp[i + 1]);
+            }
+        }
+    }
+
+    int minExtraChar(string s, vector<string>& dictionary) {
+        for(int i = 0; i < dictionary.size(); i++)
+        {
+            insert(dictionary[i]);
+        }
+        vector<int> dp(s.size() + 1);
+        for(int i = s.size() - 1; i >= 0; i--)
+        {
+            for(int j = i; j < s.size(); j++)
+            {
+                search(s, j, dp, i);
+            }
+        }
+        return s.size() - dp[0];
+    }
+};
