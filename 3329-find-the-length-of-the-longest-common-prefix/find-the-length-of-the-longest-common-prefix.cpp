c8e53ce3009138917c49d6fc30c8b779daa1abbe
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2024-09-24 20:06:44 +0700

    Time: 196 ms (97.09%) | Memory: 102.6 MB (99.88%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
@@ -1,82 +1,37 @@
-
-class TrieNode {
-public:
-    int data;
-    TrieNode* children[10];
-    bool isTerminal;
-
-    TrieNode(int ch) {
-        data = ch;
-        for (int i = 0; i < 10; i++) { children[i] = NULL; }
-        isTerminal = false;
-    }
-};
-
-class Trie {
-    TrieNode* root;
-public:
-    Trie() {
-        root = new TrieNode(-1);
-    }
-
-    void insert(int data) {
-        TrieNode* node = root;
-        int divisor = 1;
-
-        while (data / divisor >= 10) {
-            divisor *= 10;
-        }
-
-        while (divisor > 0) {
-            int digit = (data / divisor) % 10;
-            if (node->children[digit] == NULL) {
-                node->children[digit] = new TrieNode(digit);
-            }
-            node = node->children[digit];
-            divisor /= 10;
-        }
-        node->isTerminal = true;
-    }
-
-    int prefixLen(int data) {
-        TrieNode* node = root;
-        int length = 0;
-        int divisor = 1;
-
-        while (data / divisor >= 10) {
-            divisor *= 10;
-        }
-
-        while (divisor > 0) {
-            int digit = (data / divisor) % 10;
-            if (node->children[digit] == NULL) {
-                return length;
-            }
-            node = node->children[digit];
-            length++;
-            divisor /= 10;
-        }
-        return length;
-    }
-};
-
 class Solution {
-    Trie* trie = new Trie();
 public:
+    int trie[60000][10];
+    int cnt = 0;
+    void insert(string number) {
+        int u = 0;
+        for(int i = 0; i < number.size(); i++)
+        {
+            int k = number[i] - '0';
+            if(!trie[u][k]) trie[u][k] = ++cnt;
+            u = trie[u][k];
+        }
+    }
+
+    int search(string number) {
+        int u = 0;
+        for(int i = 0; i < number.size(); i++)
+        {
+            int k = number[i] - '0';
+            if(!trie[u][k]) return i;
+            u = trie[u][k];
+        }
+        return number.size();
+    }
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
-        ios::sync_with_stdio(false);
-        cin.tie(0);
-        cout.tie(0);
-
-        for (const int& num : arr1) {
-            trie->insert(num);
+        for(int i = 0; i < arr1.size(); i++)
+        {
+            insert(to_string(arr1[i]));
         }
-
-        int maxLen = 0;
-        for (const int& num : arr2) {
-            maxLen = max(trie->prefixLen(num), maxLen);
+        int ans = 0;
+        for(int i = 0; i < arr2.size(); i++)
+        {
+            ans = max(ans, search(to_string(arr2[i])));
         }
-
-        return maxLen;
+        return ans;
     }
 };
