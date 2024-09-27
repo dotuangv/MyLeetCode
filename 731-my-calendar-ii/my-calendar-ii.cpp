a79091937b0eb009cc929c7f17df12118cac4743
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2024-09-27 23:22:40 +0700

    Time: 70 ms (91.66%) | Memory: 38 MB (90.93%) - LeetSync

--- 731-my-calendar-ii/my-calendar-ii.cpp
+++ 731-my-calendar-ii/my-calendar-ii.cpp
@@ -1,48 +1,23 @@
-// Logical - Brute Approach
-
 class MyCalendarTwo {
+    vector<pair<int,int>> b;
+    vector<pair<int,int>>db;
 public:
-    vector<pair<int,int>> vp;
     MyCalendarTwo() {
-        // Don't Do anything
+        
     }
     
-    bool book(int x, int y) {
-        for(auto &val:vp){
-            int a = val.first, b = val.second;
-
-            if((x<b) and (y>a)){
-                // calculate sub-interval
-                int new_x = max(a,x);
-                int new_y = min(b,y);
-
-                // checking if the sub-interval is valid or not
-                if(check(new_x, new_y)){
-                    return false;
-                }
-            }
-
+    bool book(int start, int end) {
+        
+        for(pair<int,int> x: db){
+            if(start<x.second && end>x.first) return false;
         }
-
-        vp.push_back({x,y});
+        for(pair<int,int> x : b){
+            if(start<x.second && end>x.first){
+                db.push_back({max(start,x.first),min(end,x.second)});
+            }
+        }
+        b.push_back({start,end});
         return true;
-    }
-
-    // checking if sub-interval get overlapped more than single time
-    bool check(int x, int y){
-        int overlapp_cnt = 0;
-
-        for(int i=0;i<vp.size();i++){
-            int a = vp[i].first, b = vp[i].second;
-
-            if((x<b) and (y>a)){ // strict condition for overlapping
-                overlapp_cnt++;
-                if(overlapp_cnt == 2){
-                    return true;
-                }
-            }
-        }
-
-        return false;
+        
     }
 };
