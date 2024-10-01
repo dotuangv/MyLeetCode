be41f96153347dc357a1b78b800e3ece154b26be
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2024-10-01 20:47:53 +0700

    Time: 168 ms (18.65%) | Memory: 91 MB (5.42%) - LeetSync

--- 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.cpp
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.cpp
@@ -1,39 +1,42 @@
 class Solution {
 public:
     bool canArrange(vector<int>& arr, int k) {
-        map<int, int> mp;
-        for(int i = 0; i < arr.size(); i++) mp[arr[i] % k]++;
-        // for(auto x: mp) cout << x.first << " " << x.second << "\n";
-        for(auto x: mp)
+        unordered_map<int, int> mp;
+        unordered_set<int> se;
+        for(int i = 0; i < arr.size(); i++) {mp[arr[i] % k]++; se.insert(arr[i] % k);}
+        for(auto x: se)
         {
             int t = 1;
-            if(x.first < 0) t = -1;
-            // cout << x.first << " " << mp[x.first] << " " << mp[t*k - x.first] << " " << mp[-x.first] << "\n";
-            int y = min(mp[x.first], mp[-x.first]);
-            if(x.first == 0)
+            if(x < 0) t = -1;
+            int y = min(mp[x], mp[-x]);
+            if(x == 0)
             {
-                mp[x.first] -= y/2;
-                mp[-x.first] -= y/2;
+                mp[x] -= y/2;
+                mp[-x] -= y/2;
             }else
             {
-                mp[x.first] -= y;
-                mp[-x.first] -= y;
+                mp[x] -= y;
+                mp[-x] -= y;
             }
-            int z = min(mp[x.first], mp[t*k - x.first]);
-            // cout << x.first << " " << mp[x.first] << " " << mp[t*k - x.first] << " " << mp[-x.first] << "\n";
-            if(x.first == t*k - x.first)
+            int z = min(mp[x], mp[t*k - x]);
+            if(x == t*k - x)
             {
-                mp[x.first] -= z/2;
-                mp[t*k - x.first] -= z/2;
+                mp[x] -= z/2;
+                mp[t*k - x] -= z/2;
             } else
             {
-                mp[x.first] -= z;
-                mp[t*k - x.first] -= z;
+                mp[x] -= z;
+                mp[t*k - x] -= z;
             }
-            // cout << x.first << " " << mp[x.first] << " " << mp[t*k - x.first] << " " << mp[-x.first] << "\n";
-            if(mp[x.first]) return false;
+            if(mp[x]) return false;
         }
         return true;
         
     }
 };
+static const int KDS = []() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}();
