f21282eae53f57c274cec2c1480c9be465eed0d3
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2024-10-04 22:11:35 +0700

    Time: 56 ms (87.00%) | Memory: 56.3 MB (92.60%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     long long dividePlayers(vector<int>& skill) {
-        unordered_map<int, long long> mp;
+        vector<long long> mp(1001);
         long long ans = 0, sum = 0;
         for(auto x: skill) {mp[x]++; sum += x;}
         if(sum % (skill.size()/2)) return -1;
         sum = sum/(skill.size()/2);
-        for(auto x: mp)
+        for(int x = 1; x <= 1000; x++)
         {
-            if(mp[x.first])
+            if(mp[x] != 0)
             {
-                if(!mp.contains(sum - x.first)) return -1;
+                if(sum - x < 0) return -1;
                 else
                 {
-                    if(x.first*2 == sum)
+                    if(x*2 == sum)
                     {
-                        if(mp[x.first] % 2 != 0) return -1;
-                        else ans += mp[x.first]/2 * x.first * x.first;
+                        if(mp[x] % 2 != 0) return -1;
+                        else ans += mp[x]/2 * x * x;
                     }else 
                     {
-                        if(mp[x.first] != mp[sum - x.first]) return -1;
-                        else ans += mp[x.first] * x.first * (sum - x.first);
-                        mp[x.first] = 0;
-                        mp[sum - x.first] = 0;
+                        if(mp[x] != mp[sum - x]) return -1;
+                        else ans += mp[x] * x * (sum - x);
+                        mp[x] = 0;
+                        mp[sum - x] = 0;
                     }
                 }
             }
