540dfad9342c1f62d9ad781f9ec20532a76938b6
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2024-10-11 00:44:33 +0700

    Time: 32 ms (81.08%) | Memory: 46.9 MB (42.36%) - LeetSync

--- 1002-maximum-width-ramp/maximum-width-ramp.cpp
+++ 1002-maximum-width-ramp/maximum-width-ramp.cpp
@@ -1,26 +1,49 @@
 class Solution {
 public:
     int maxWidthRamp(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         int n = nums.size();
-        stack<int> s;
+        stack<int> st;
         
-        // Step 1: Build a decreasing stack of indices
-        for (int i = 0; i < n; ++i) {
-            if (s.empty() || nums[s.top()] > nums[i]) {
-                s.push(i);
+        st.push(0);
+
+        for(int i = 1; i < n; i++){
+            if(nums[st.top()] > nums[i]){
+                st.push(i);
             }
         }
-        
-        int maxWidth = 0;
-        
-        // Step 2: Traverse from the end and find maximum width ramp
-        for (int j = n - 1; j >= 0; --j) {
-            while (!s.empty() && nums[s.top()] <= nums[j]) {
-                maxWidth = max(maxWidth, j - s.top());
-                s.pop();
+
+        int ans = 0;
+
+        for(int i = n-1; i >= 0; ){
+
+            while(!st.empty() and nums[st.top()] <= nums[i]){
+                ans = max(ans, i - st.top());
+                st.pop();
             }
+
+            if(st.empty()) break;
+
+            i--;
         }
-        
-        return maxWidth;
+
+        return ans;
+
     }
 };
+
+/*
+https://www.youtube.com/watch?v=_TkAgiKixAk&ab_channel=Codekitchen%7CNewtonschool
+
+use the stack to solve it
+
+n^2 for brute force
+
+put the numbers in decreading order in the stack for all the elements
+
+then just iterate from right to compare with the top element
+
+whenever you found someone to compare with the top: remove it because it can not again contribute in maximum
+
+*/
