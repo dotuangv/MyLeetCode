bb5eb9b4d2ba91f033384f7b1dc5ff62dc31a678
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2024-10-29 00:29:39 +0700

    Time: 7 ms (100.00%) | Memory: 87 MB (92.28%) - LeetSync

--- 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.cpp
+++ 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.cpp
@@ -10,13 +10,13 @@ auto init = []() {
 class Solution {
 public:
     int longestSquareStreak(vector<int>& nums) {
-        unordered_map<int, bool> mp;
+        vector<bool> mp(100001);
         for(auto x: nums) mp[x] = true;
         int ans = 0;
         for(auto x: nums)
         {
             int res = 0, y = x;
-            while(mp[y])
+            while(y <= 100000 && mp[y])
             {
                 res++;
                 if(y > 1e9/y) break;
