4f8d3890b9ac4e06ce3ce6c70793e0245b2f941d
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2024-10-30 22:46:21 +0700

    Time: 14 ms (33.79%) | Memory: 30.5 MB (11.89%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -1,16 +1,3 @@
-// Constraints:
-
-// m == matrix.length
-// n == matrix[i].length
-// 1 <= m, n <= 300
-// matrix[i][j] is '0' or '1'.
-// Array
-// Dynamic Programming
-// Matrix
-
-#include <vector>
-#include <iostream>
-
 const static auto fast = []
 {
     std::ios_base::sync_with_stdio(false);
@@ -18,44 +5,33 @@ const static auto fast = []
     std::cout.tie(nullptr);
     return 0;
 }();
-
-
-class Solution
-{
+class Solution {
 public:
-    /**
-     * @brief Calculates the area of the largest square of '1's in the given matrix.
-     * 
-     * @details 
-     * This function uses dynamic programming to solve the problem. It iterates over the matrix and for each cell that contains '1', 
-     * it finds the size of the square that can be made using this cell as the bottom right corner by checking its adjacent cells. 
-     * It keeps track of the maximum size of the square found so far.
-     * 
-     * Time complexity: O(m*n) where m is the number of rows and n is the number of columns in the matrix.
-     * Space complexity: O(m*n) as a 2D vector of size m*n is used for dynamic programming.
-     * 
-     * @param matrix The input matrix of '0's and '1's.
-     * @return The area of the largest square of '1's in the matrix.
-     */
-    int maximalSquare(std::vector<std::vector<char>> &matrix)
-    {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        int maxSideLength = 0;  // This holds the side length of the maximum square found so far. There is no need to do the multiplication to find area until it is time to return the result.
-        std::vector<std::vector<int>> dp(m + 1, std::vector<int>(n + 1, 0));
-
-        for (int row = 1; row <= m; row++)
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int ans = 0;
+        vector<vector<int>> dp(matrix.size(), vector<int> (matrix[0].size()));
+        for(int i = 0; i < matrix.size(); i++) 
         {
-            for (int col = 1; col <= n; col++)
+            dp[i][0] = matrix[i][0] - '0';
+            if(!ans) ans = dp[i][0];
+        }
+        for(int i = 0; i < matrix[0].size(); i++)
+        {
+            dp[0][i] = matrix[0][i] - '0';
+            if(!ans) ans = dp[0][i];
+        }
+        for(int i = 1; i < matrix.size(); i++)
+        {
+            for(int j = 1; j < matrix[0].size(); j++)
             {
-                if (matrix[row - 1][col - 1] == '1')
+                dp[i][j] = matrix[i][j] - '0';
+                if(dp[i][j])
                 {
-                    dp[row][col] = std::min({dp[row - 1][col], dp[row][col - 1], dp[row - 1][col - 1]}) + 1;
-                    maxSideLength = std::max(maxSideLength, dp[row][col]);
+                    dp[i][j] = min({dp[i][j - 1], dp[i - 1][j - 1], dp[i - 1][j]}) + 1;
                 }
+                ans = max(ans, dp[i][j]);
             }
         }
-
-        return maxSideLength * maxSideLength;
+        return ans*ans;
     }
 };
