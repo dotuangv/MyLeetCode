36b21e4b6c34e2ad227c769ec2ca31961e1d192e
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2024-10-30 10:11:13 +0700

    Time: 405 ms (30.40%) | Memory: 82.2 MB (13.03%) - LeetSync

--- 3630-total-characters-in-string-after-transformations-ii/total-characters-in-string-after-transformations-ii.cpp
+++ 3630-total-characters-in-string-after-transformations-ii/total-characters-in-string-after-transformations-ii.cpp
@@ -1,59 +1,60 @@
+#include <vector>
+#include <string>
+using namespace std;
+
 #define ll long long
+
 class Solution {
 public:
     int MOD = 1e9 + 7;
-    vector<vector<ll>> Mul(vector<vector<ll>> &vt1, vector<vector<ll>> vt2)
-    {
-        vector<vector<ll>> result(26, vector<ll> (26));
-        for(int i = 0; i < 26; i++)
-        {
-            for(int j = 0; j < 26; j++)
-            {
-                for(int k = 0; k < 26; k++)
-                {
-                    result[i][j] += (vt1[i][k] % MOD) * (vt2[k][j] % MOD);
-                    result[i][j] %= MOD;
+
+    vector<vector<ll>> Mul(const vector<vector<ll>> &vt1, const vector<vector<ll>> &vt2) {
+        vector<vector<ll>> result(26, vector<ll>(26, 0));
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < 26; j++) {
+                ll sum = 0;
+                for (int k = 0; k < 26; k++) {
+                    sum = (sum + vt1[i][k] * vt2[k][j]) % MOD;
                 }
+                result[i][j] = sum;
             }
         }
         return result;
     }
 
-    vector<vector<ll>> pow(int n, vector<vector<ll>> &vt)
-    {
-        vector<vector<ll>> result(26, vector<ll> (26));
-        for(int i = 0; i < 26; i++) result[i][i] = 1;
-        while(n)
-        {
-            if(n % 2)
-            {
+    vector<vector<ll>> pow(int n, vector<vector<ll>> &vt) {
+        vector<vector<ll>> result(26, vector<ll>(26, 0));
+        for (int i = 0; i < 26; i++) result[i][i] = 1; // Identity matrix
+        
+        while (n > 0) {
+            if (n % 2 == 1) {
                 result = Mul(result, vt);
             }
             vt = Mul(vt, vt);
-            n/=2;
+            n /= 2;
         }
         return result;
     }
 
     int lengthAfterTransformations(string s, int t, vector<int>& nums) {
-        vector<vector<ll>> vt1(26, vector<ll> (26));
-        for(auto x: s) vt1[x - 'a'][x - 'a']++;
-        vector<vector<ll>> vt2(26, vector<ll> (26));
-        for(int i = 0; i < nums.size(); i++)
-        {
-            for(int j = 1; j <= nums[i]; j++)
-            {
-                vt2[i][(i + j)%26]++;
+        vector<vector<ll>> vt1(26, vector<ll>(26, 0));
+        for (auto x : s) vt1[x - 'a'][x - 'a']++;
+
+        vector<vector<ll>> vt2(26, vector<ll>(26, 0));
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = 1; j <= nums[i]; j++) {
+                vt2[i][(i + j) % 26]++;
             }
         }
+
         vt1 = Mul(vt1, pow(t, vt2));
+
         int ans = 0;
-        for(int i = 0; i < 26; i++)
-            for(int j = 0; j < 26; j++) {
-                ans += vt1[i][j];
-                ans %= MOD;
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < 26; j++) {
+                ans = (ans + vt1[i][j]) % MOD;
             }
+        }
         return ans;
-        
     }
 };
