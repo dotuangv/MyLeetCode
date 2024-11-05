e40d3c0392da5d5a07820c399aa8152d5e338246
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2024-11-05 09:39:23 +0700

    Time: 4 ms (84.18%) | Memory: 26.5 MB (64.77%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
+    int dp[2002];
+    int end_to_min_len[2002];
     int findLongestChain(vector<vector<int>>& pairs) {
-        sort(pairs.begin(), pairs.end());
-        int dp[pairs.size()];
-        dp[0] = 1;
-        int ans = 1;
-        for(int i = 1; i < pairs.size(); i++)
-        {
-            dp[i] = 1;
-            for(int j = i - 1; j >= 0; j--)
-            {
-                if(pairs[i][0] > pairs[j][1]) dp[i] = max(dp[i], dp[j] + 1);
-            }
-            ans = max(ans, dp[i]);
+        memset(dp, 0, sizeof(dp));
+        memset(end_to_min_len, 0, sizeof(end_to_min_len));
+        int pmax = -1001;
+        int pmin = 1001;
+        for(const auto& p: pairs){
+            end_to_min_len[p[1]+1000] = end_to_min_len[p[1]+1000] == 0 ? p[1]-p[0] :
+            min(end_to_min_len[p[1]+1000], p[1]-p[0]);
+            pmin = min(p[0], pmin);
+            pmax = max(p[1], pmax);
         }
-        return ans;
+        for(int i = pmin; i <= pmax; i++){
+            dp[i+1001] = dp[i+1000];
+            if(end_to_min_len[i+1000]){
+                dp[i+1001] = max(dp[i+1001], dp[i+1000-end_to_min_len[i+1000]]+1);
+            }
+        }
+        return dp[pmax+1001];
     }
 };
