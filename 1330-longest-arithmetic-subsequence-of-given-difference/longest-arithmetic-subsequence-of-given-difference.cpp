9c4c1c0800ab80fb506b9e614df749dd288f52dd
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2024-11-05 10:10:35 +0700

    Time: 2 ms (98.98%) | Memory: 55.8 MB (97.43%) - LeetSync

--- 1330-longest-arithmetic-subsequence-of-given-difference/longest-arithmetic-subsequence-of-given-difference.cpp
+++ 1330-longest-arithmetic-subsequence-of-given-difference/longest-arithmetic-subsequence-of-given-difference.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int longestSubsequence(vector<int>& arr, int difference) {
-        unordered_map<int, int> dp;
+        vector<int> dp(20002);
         int ans = 1;
-        dp[arr[0]] = 1;
+        dp[arr[0] + 10000] = 1;
         for(int i = 1; i < arr.size(); i++)
         {
             // dp[arr[i]] = max(dp[arr[i]], 1);
-            dp[arr[i]] = max(dp[arr[i]], 1 + dp[arr[i] - difference]);
-            ans = max(ans, dp[arr[i]]);
+            if(arr[i] - difference + 10000 >= 0 && arr[i] - difference + 10000 <= 20001)
+                dp[arr[i] + 10000] = max(dp[arr[i] + 10000], 1 + dp[arr[i] - difference + 10000]);
+            else dp[arr[i] + 10000] = 1;
+            ans = max(ans, dp[arr[i] + 10000]);
         }
         return ans; 
     }
