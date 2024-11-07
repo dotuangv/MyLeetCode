27d4fe958030766af48239c6cbbd7c30fd65dfc7
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2024-11-07 11:34:26 +0700

    Time: 267 ms (77.54%) | Memory: 95.9 MB (82.54%) - LeetSync

---
+++ 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int minTimeToReach(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int startX = 0, startY = 0;
+         // Khởi tạo ma trận khoảng cách với giá trị vô cùng
+    vector<vector<int>> dist(m, vector<int>(n, numeric_limits<int>::max()));
+    dist[startX][startY] = 0;
+
+    // Hàng đợi ưu tiên để lưu các ô (khoảng cách, tọa độ x, tọa độ y)
+    priority_queue<tuple<int, int, int, int>, vector<tuple<int, int, int, int>>, greater<>> pq;
+    pq.emplace(0, startX, startY, 0);
+
+    // Các hướng di chuyển: lên, xuống, trái, phải
+    vector<pair<int, int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+    while (!pq.empty()) {
+        auto [currentDist, x, y, z] = pq.top();
+        pq.pop();
+        int point = z % 2 + 1;
+        // Nếu đã đến đích
+        if (x == m - 1 && y == n - 1) {
+            return currentDist;
+        }
+
+        // Bỏ qua nếu tìm thấy đường tốt hơn trước đó
+        if (currentDist > dist[x][y]) {
+            continue;
+        }
+
+        // Duyệt các ô kề
+        for (const auto& [dx, dy] : directions) {
+            int nx = x + dx, ny = y + dy;
+            if (nx >= 0 && nx < m && ny >= 0 && ny < n) {  // Kiểm tra giới hạn của ma trận
+                int newDist = max(currentDist, grid[nx][ny]) + point;
+                if (newDist < dist[nx][ny]) {
+                    dist[nx][ny] = newDist;
+                    pq.emplace(newDist, nx, ny, point);
+                }
+            }
+        }
+    }
+    return dist[m - 1][n - 1];
+    }
+};
