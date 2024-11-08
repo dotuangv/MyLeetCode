cd5164ebb3635e88bc42868404dda9f9a577de2a
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2024-11-08 22:53:14 +0700

    Time: 6 ms (39.05%) | Memory: 102.8 MB (6.11%) - LeetSync

--- 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.cpp
+++ 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.cpp
@@ -4,28 +4,11 @@ public:
         vector<int> pre(nums.size());
         pre[0] = nums[0];
         for(int i = 1; i < nums.size(); i++) pre[i] = pre[i - 1] ^ nums[i];
-        bitset<21> y(pow(2, maximumBit) - 1);
+        int res = pow(2, maximumBit) - 1;
         vector<int> ans;
         for(int i = nums.size() - 1; i >= 0; i--)
         {
-            bitset<21> x(pre[i]);
-            bool ok = false;
-            bitset<21> tmp(0);
-            for(int j = 20; j >= 0; j--)
-            {
-                if(x[j] == 0)
-                {
-                    if(y[j] == 1) tmp[j] = 1;
-                    else
-                    {
-                        if(ok) tmp[j] = 1;
-                    }
-                }else
-                {
-                    if(y[j] == 1) ok = true;
-                }
-            }
-            ans.push_back(tmp.to_ulong());
+            ans.push_back((pre[i] | res)^pre[i]);
         }
         return ans;
     }
