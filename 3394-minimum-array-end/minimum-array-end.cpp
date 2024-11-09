d1f4b7b4a8be5e72b9a494605a8be7b937292ad9
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2024-11-09 10:39:11 +0700

    Time: 2 ms (36.61%) | Memory: 9.6 MB (12.24%) - LeetSync

--- 3394-minimum-array-end/minimum-array-end.cpp
+++ 3394-minimum-array-end/minimum-array-end.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
     long long minEnd(int n, int x) {
-        if(n == 1) return x;
-        bitset<64> a(x);
+        bitset<64> a(x), b(n - 1);
         vector<int> vt;
         for(int i = 0; i < 64; i++)
-        {
             if(a[i] == 0) vt.push_back(i);
-        }
-        bitset<64> b(n - 1);
         int j = 0;
         for(int i = 0; i < 64; i++)
         {
             if(j >= vt.size()) break;
             a[vt[j++]] = b[i];
         }
-        long long ans = a.to_ulong();
-
-        return ans;
+        return a.to_ulong();
         
     }
 };
