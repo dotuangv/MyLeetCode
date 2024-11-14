9666f09ff684abbee69d800c8b19fd234e362a22
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2024-11-14 10:22:08 +0700

    Time: 446 ms (40.19%) | Memory: 126.4 MB (51.79%) - LeetSync

--- 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.cpp
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.cpp
@@ -1,17 +1,55 @@
 class Solution {
 public:
-    int maxFrequency(vector<int>& nums, int k, int numOperations) {
-        vector<int> mp(300001), pre(300001);
-        for(auto x: nums) mp[x + 100000]++;
-        for(int i = 1; i <= 300000; i++){
-            pre[i] = pre[i - 1] + mp[i];
+    
+    int upper(long long x, vector<int> &nums)
+    {
+        int l = 0, r = nums.size() - 1;
+        while(r > l + 1)
+        {
+            int mid = (l + r)/2;
+            if(nums[mid] <= x) l = mid;
+            else r = mid;
         }
+        if(nums[r] <= x) return r;
+        else if(nums[l] <= x) return l;
+        return -1;
+    }
+    
+    int lower(long long x, vector<int> &nums)
+    {
+        int l = 0, r = nums.size() - 1;
+        while(r > l + 1)
+        {
+            int mid = (l + r)/2;
+            if(nums[mid] >= x) r = mid;
+            else l = mid;
+        }
+        if(nums[l] >= x) return l;
+        else if(nums[r] >= x) return r;
+        return -1;
+    }
+    
+    int maxFrequency(vector<int>& nums, int k, int numOperations) {
+        sort(nums.begin(), nums.end());
+        map<int, int> mp;
+        for(auto x: nums) mp[x]++;
         int ans = 0;
-        for(int i = 100000; i <= 200000; i++){
-            int res = pre[i + k];
-            if(i != k) res -= pre[i - k - 1];
-            ans = max(ans, mp[i] + min(numOperations, res - mp[i]));
+        for(int i = 0; i < nums.size(); i++)
+        {
+            int r = upper(nums[i] + k, nums), l = lower(nums[i] - k, nums);
+            int res = 0;
+            if(l != -1) res += i - l;
+            if(r != -1) res += r - i;
+            res++;
+            ans = max(ans, mp[nums[i]] + min(res - mp[nums[i]], numOperations));
+        }
+        
+        for(int i = 0; i < nums.size(); i++)
+        {
+            int r = upper(nums[i] + 2*k, nums);
+            ans = max(ans, min(r - i + 1, numOperations));
         }
         return ans;
+        
     }
 };
