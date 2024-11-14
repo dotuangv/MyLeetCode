224c9e77c9ddd6b465c0ff439f9499e8d319a74d
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2024-11-14 11:08:57 +0700

    Time: 398 ms (10.11%) | Memory: 148.2 MB (5.51%) - LeetSync

--- 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.cpp
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
 
-    bool isGood(int x, int n, vector<int> quantities){
+    bool isGood(int x, int n, vector<int> q){
         int i = 0, j = 0;
         while(j < n){
-            if(quantities[i] == 0) i++;
-            if(i == quantities.size()) return true;
-            quantities[i] = max(0, quantities[i] - x);
+            if(q[i] == 0) i++;
+            if(i == q.size()) return true;
+            q[i] = max(0, q[i] - x);
             j++;
         }
-        return quantities.back() == 0;
+        return q.back() == 0;
     }
 
-    int minimizedMaximum(int n, vector<int>& quantities) {
-        int l = 0, r = 100000;
+    int minimizedMaximum(int n, vector<int>& q) {
+        int l = 1, r = 1;
+        for(int i=0;i<q.size();i++) {
+            r = max(r, q[i]);
+        }
         while(r > l + 1){
             int mid = (l + r)/2;
-            if(isGood(mid, n, quantities)) r = mid;
+            if(isGood(mid, n, q)) r = mid;
             else l = mid;
         }
-        if(isGood(l, n, quantities)) return l;
+        if(isGood(l, n, q)) return l;
         return r;
     }
 };
