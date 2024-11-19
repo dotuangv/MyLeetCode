63d264a55da557b1f764589e562971e4f759915a
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2024-11-19 23:20:41 +0700

    Time: 35 ms (98.59%) | Memory: 81.2 MB (91.39%) - LeetSync

--- 354-russian-doll-envelopes/russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/russian-doll-envelopes.cpp
@@ -1,39 +1,42 @@
 class Solution {
 public:
-    vector<int> st;
 
-    void update(int id, int l, int r, int pos, int val){
-        if(r < pos || l > pos) return;
-        if(l == r){
-            st[id] = max(st[id], val);
-            return;
+    int upper(int x, vector<int> &dp){
+        int l = 0, r = dp.size() - 1;
+        while(r > l + 1){
+            int mid = (l + r)/2;
+            if(dp[mid] >= x) r = mid;
+            else l = mid;
         }
-        int mid = (l + r)/2;
-        update(id * 2 + 1, l, mid, pos, val);
-        update(id * 2 + 2, mid + 1, r, pos, val);
-        st[id] = max(st[id*2 + 1], st[id*2 + 2]);
+        if(dp[l] >= x) return l;
+        return r;
     }
 
-    int get(int id, int l, int r, int u, int v){
-        if(r < u || l > v) return 0;
-        if(u <= l && r <= v) return st[id];
-        int mid = (l + r)/2;
-        return max(get(id*2 + 1, l, mid, u, v), get(id*2 + 2, mid + 1, r, u, v));
-    }
-
-    int maxEnvelopes(vector<vector<int>>& envelopes) {
-        int n = 0;
-        for(auto x: envelopes) n = max(n, x[1]);
-        st.resize(4 * n, 0);
-        sort(envelopes.begin(), envelopes.end(), [](vector<int> &a, vector<int> &b) {
+    int maxEnvelopes(vector<vector<int>>& ev) {
+        sort(ev.begin(), ev.end(), [](vector<int> &a, vector<int> &b) {
             return a[0] == b[0] ? a[1] > b[1] : a[0] < b[0];
         });
-        int ans = 1;
-        for(int i = 0; i < envelopes.size(); i++){
-            int x = get(0, 0, n - 1, 0, envelopes[i][1] - 1);
-            ans = max(x + 1, ans);
-            update(0, 0, n - 1, envelopes[i][1], x + 1);
+        // for(int i = 0; i < ev.size(); i++){
+        //     cout << ev[i][1] << " ";
+        // }
+        // cout << "\n";
+        // for(int i = 0; i < ev.size(); i++){
+        //     cout << ev[i][0] << "  ";
+        // }
+        // cout << "\n";
+        vector<int> dp;
+        dp.push_back(ev[0][1]);
+        for(int i = 1; i < ev.size(); i++){
+            if(ev[i][1] > dp.back()){
+                dp.push_back(ev[i][1]);
+            }else
+            {
+                int j = upper(ev[i][1], dp);
+                dp[j] = ev[i][1];
+            }
+            // for(auto x: dp) cout << x << " ";
+            // cout << "\n";
         }
-        return ans;
+        return dp.size();
     }
 };
