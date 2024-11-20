786bbe67b12c32c1d5dd8a7d2e47bd79421685ce
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2024-11-20 19:21:15 +0700

    Time: 19 ms (49.76%) | Memory: 23.9 MB (7.45%) - LeetSync

--- 2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.cpp
+++ 2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.cpp
@@ -1,49 +1,46 @@
 class Solution {
 public:
 
-    void update(int &a, int &b, int &c, char ch){
-        if(ch == 'a') a++;
-        else if(ch == 'b') b++;
-        else if(ch == 'c') c++;
+    void update(int &a, int &b, int &c, char ch, int i, vector<int> &A, vector<int> &B, vector<int> &C){
+        if(ch == 'a') {
+            a++;
+            A[a] = i;
+        }else if(ch == 'b') {
+            b++;
+            B[b] = i;
+        }else if(ch == 'c') {
+            c++;
+            C[c] = i;
+        }
     }
 
-    int BS(int x, int j, vector<int> &vt){
-        int l = j, r = vt.size() - 1;
-        while(r > l + 1){
-            int mid = (l + r)/2;
-            if(vt[mid] >= x) r = mid;
-            else l = mid;
+    void update2(int &a, int &b, int &c, char ch){
+        if(ch == 'a') {
+            a++;
+        }else if(ch == 'b') {
+            b++;
+        }else if(ch == 'c') {
+            c++;
         }
-        if(vt[l] >= x) return l;
-        return r;
     }
 
     int takeCharacters(string s, int k) {
         if(k == 0) return 0;
         int n = s.size();
-        vector<int> a(n), b(n), c(n);
+        vector<int> A(n + 1), B(n + 1), C(n + 1);
         int ans = 1e9;
-        update(a[0], b[0], c[0], s[0]);
-        for(int i = 1; i < n; i++){
-            a[i] = a[i - 1];
-            b[i] = b[i - 1];
-            c[i] = c[i - 1];
-            update(a[i], b[i], c[i], s[i]);
-            if(a[i] >= k && b[i] >= k && c[i] >= k) ans = min(ans, i + 1);
+        int a = 0, b = 0, c = 0;
+        for(int i = 0; i < n; i++){
+            update(a, b, c, s[i], i + 1, A, B, C);
+            if(a >= k && b >= k && c >= k) ans = min(ans, i + 1);
         }
         if(ans == 1e9) return -1;
-        int A = 0, B = 0, C = 0;
+        a = 0, b = 0, c = 0;
         for(int i = n - 1; i >= 0; i--){
-            update(A, B, C, s[i]);
-            int x = max(0, k - A), y = max(0, k - B), z = max(0, k - C);
-            if(x == 0 && y == 0 && z == 0){
-                ans = min(ans, n - i);
-                break;
-            }
-            int j = BS(x, 0, a);
-            j = BS(y, j, b);
-            j = BS(z, j, c);
-            ans = min(ans, j + 1 + n - i);
+            update2(a, b, c, s[i]);
+            int x = max(0, k - a), y = max(0, k - b), z = max(0, k - c);
+            int j = max({A[x], B[y], C[z]});
+            ans = min(ans, n - i + j);
         }
         return ans;
 
