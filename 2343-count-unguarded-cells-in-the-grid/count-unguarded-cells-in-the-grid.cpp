54b3efbd95a9162b5d30e393e09d32ee12b3ba58
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2024-11-21 10:14:09 +0700

    Time: 16 ms (98.97%) | Memory: 150.8 MB (96.08%) - LeetSync

--- 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.cpp
+++ 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.cpp
@@ -1,62 +1,30 @@
 class Solution {
 public:
     int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
-        vector<vector<int>> matrix(m, vector<int> (n));
-        for(auto x: guards){
-            matrix[x[0]][x[1]] = 1;
+        int g[m][n];
+        memset(g, 0, sizeof(g));
+        for (auto& e : guards) {
+            g[e[0]][e[1]] = 2;
         }
-        for(auto x: walls){
-            matrix[x[0]][x[1]] = 2;
+        for (auto& e : walls) {
+            g[e[0]][e[1]] = 2;
         }
-        for(int i = 0; i < m; i++){
-            int l = 0;
-            bool ok = false;
-            for(int j = 0; j < n; j++){
-                if(ok && !matrix[i][j]){
-                    matrix[i][j] = 3;
-                    l++;
-                    continue;
-                }
-                if(matrix[i][j] == 1){
-                    for(int k = l; k < j; k++){
-                        if(!matrix[i][k]) matrix[i][k] = 3;
-                    }
-                    ok = true;
-                    l = j + 1;
-                }else if(matrix[i][j] == 2){
-                    l = j + 1;
-                    ok = false;
-                }
-            }
-        }
-        for(int j = 0; j < n; j++){
-            int l = 0;
-            bool ok = false;
-            for(int i = 0; i < m; i++){
-                if(ok && !matrix[i][j]){
-                    matrix[i][j] = 3;
-                    l++;
-                    continue;
-                }
-                if(matrix[i][j] == 1){
-                    for(int k = l; k < i; k++){
-                        if(!matrix[k][j]) matrix[k][j] = 3;
-                    }
-                    l = i + 1;
-                    ok = true;
-                }else if(matrix[i][j] == 2){
-                    l = i + 1;
-                    ok = false;
+        int dirs[5] = {-1, 0, 1, 0, -1};
+        for (auto& e : guards) {
+            for (int k = 0; k < 4; ++k) {
+                int x = e[0], y = e[1];
+                int a = dirs[k], b = dirs[k + 1];
+                while (x + a >= 0 && x + a < m && y + b >= 0 && y + b < n && g[x + a][y + b] < 2) {
+                    x += a;
+                    y += b;
+                    g[x][y] = 1;
                 }
             }
         }
         int ans = 0;
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(!matrix[i][j]) ans++;
-            }
+        for (auto& row : g) {
+            ans += count(row, row + n, 0);
         }
         return ans;
-
     }
 };
