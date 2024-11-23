4074787a2fbf6e06615552df877346f6e35398ab
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2024-11-23 15:25:37 +0700

    Time: 223 ms (20.00%) | Memory: 56.4 MB (25.37%) - LeetSync

---
+++ 1972-rotating-the-box/rotating-the-box.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
+        int m = box.size(), n = box[0].size();
+        for(int i = 0; i < box.size(); i++){
+            for(int j = n - 1; j >= 0; j--){
+                if(box[i][j] == '#'){
+                    for(int k = j; k < n - 1; k++){
+                        if(box[i][k + 1] == '.') swap(box[i][k], box[i][k + 1]);
+                        else break;
+                    }
+                }
+            }
+        }
+        vector<vector<char>> ans(n, vector<char> (m));
+        for(int i = 0; i < n; i++){
+            for(int j = m - 1; j >= 0; j--) ans[i][m - j - 1] = box[j][i];
+        }
+        return ans;
+    }
+};
