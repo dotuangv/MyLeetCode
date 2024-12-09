9302dca930ae0e47abaf150a7ff1beafff29f72a
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2024-12-09 10:12:59 +0700

    Time: 4 ms (87.50%) | Memory: 126.7 MB (60.13%) - LeetSync

--- 3427-special-array-ii/special-array-ii.cpp
+++ 3427-special-array-ii/special-array-ii.cpp
@@ -3,14 +3,14 @@ public:
     vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
         int n = nums.size();
         vector<bool> ans(queries.size());
-        vector<int> vt(n);
-        vt[0] = 0;
+        vector<int> pre(n);
+        pre[0] = 0;
         for(int i = 1; i < n; i++){
-            if(nums[i] % 2 != nums[i - 1]%2) vt[i] = vt[i - 1];
-            else vt[i] = i;
+            if(nums[i] % 2 != nums[i - 1]%2) pre[i] = pre[i - 1];
+            else pre[i] = i;
         }
         for(int i = 0; i < queries.size(); i++){
-            if(queries[i][0] >= vt[queries[i][1]]) ans[i] = true;
+            if(queries[i][0] >= pre[queries[i][1]]) ans[i] = true;
         }
         return ans;
     }
