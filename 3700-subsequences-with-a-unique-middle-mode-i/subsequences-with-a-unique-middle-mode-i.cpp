6062e0613e822c6a37f5a729f2d059829cd1a8ef
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2024-12-22 00:17:55 +0700

    Time: 1000 ms (18.18%) | Memory: 119.6 MB (9.09%) - LeetSync

--- 3700-subsequences-with-a-unique-middle-mode-i/subsequences-with-a-unique-middle-mode-i.cpp
+++ 3700-subsequences-with-a-unique-middle-mode-i/subsequences-with-a-unique-middle-mode-i.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     long long mod = 1e9 + 7;
+    int ans = 0;
     long long mul(long long x, long long y){
         return ((x % mod) * (y % mod))%mod;
     }
     long long C2(long long n){
         return (n * (n - 1)/2) % mod;
     }
-    void update(int &ans, int x){
+    void update(int x){
         ans = (ans%mod + x%mod + mod) % mod;
     }
 
     int subsequencesWithMiddleMode(vector<int>& nums) {
-        int ans = 0;
         vector<pair<int, int>> vt;
         for(int i = 0; i < nums.size(); i++){
             vt.push_back({nums[i], i});
@@ -39,36 +39,36 @@ public:
             long long r0 = C2(m - mp2[nums[i]]), r1 = mul(mp2[nums[i]], m - mp2[nums[i]]), r2 = C2(mp2[nums[i]]);
             // mode mid = 2
             // 1 1 0
-            update(ans, mul(l1, r0));
+            update(mul(l1, r0));
             for(int x = 0; x < k; x++){
                 if(x != nums[i]){
-                    update(ans, -mul(mul(n - mp1[x] - mp1[nums[i]], mp1[nums[i]]), C2(mp2[x])));
-                    update(ans, - mul(mul(mp1[x], mp1[nums[i]]), mul(mp2[x], m - mp2[x] - mp2[nums[i]])) - mul(mul(mp1[x], mp1[nums[i]]), C2(mp2[x])));
+                    update(- mul(mul(n - mp1[x] - mp1[nums[i]], mp1[nums[i]]), C2(mp2[x])));
+                    update(- mul(mul(mp1[x], mp1[nums[i]]), mul(mp2[x], m - mp2[x] - mp2[nums[i]])) - mul(mul(mp1[x], mp1[nums[i]]), C2(mp2[x])));
                 }
             }
             // 0 1 1
-            update(ans, mul(l0, r1));
+            update(mul(l0, r1));
             for(int x = 0; x < k; x++){
                 if(x != nums[i]){
-                    update(ans, - mul(mul(m - mp2[x] - mp2[nums[i]], mp2[nums[i]]), C2(mp1[x])));
-                    update(ans, - mul(mul(mp2[x], mp2[nums[i]]), mul(mp1[x], n - mp1[x] - mp1[nums[i]])) - mul(mul(mp2[x], mp2[nums[i]]), C2(mp1[x])));
+                    update(- mul(mul(m - mp2[x] - mp2[nums[i]], mp2[nums[i]]), C2(mp1[x])));
+                    update(- mul(mul(mp2[x], mp2[nums[i]]), mul(mp1[x], n - mp1[x] - mp1[nums[i]])) - mul(mul(mp2[x], mp2[nums[i]]), C2(mp1[x])));
                 }
             }
             // mode mid = 3
             // 2 1 0
-            update(ans, mul(l2, r0));
+            update(mul(l2, r0));
             // 0 1 2
-            update(ans, mul(l0, r2));
+            update(mul(l0, r2));
             // 1 1 1
-            update(ans, mul(l1, r1));
+            update(mul(l1, r1));
             // mode mid = 4
             // 2 1 1
-            update(ans, mul(l2, r1));
+            update(mul(l2, r1));
             // 1 1 2
-            update(ans, mul(l1, r2));
+            update(mul(l1, r2));
             // mode mid = 5
             // 2 1 2
-            update(ans, mul(l2, r2));
+            update(mul(l2, r2));
         }
         return ans;
     }
