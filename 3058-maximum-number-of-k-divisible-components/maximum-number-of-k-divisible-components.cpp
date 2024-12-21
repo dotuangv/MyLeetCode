e1a264f8003e7df2d2c793284caec3eaba0628fc
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2024-12-21 21:13:21 +0700

    Time: 107 ms (72.15%) | Memory: 173.4 MB (84.81%) - LeetSync

--- 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.cpp
+++ 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.cpp
@@ -1,51 +1,28 @@
 class Solution {
 public:
+    int ans = 0;
+    long long dfs(int i, vector<vector<int>>& path, vector<int>& values, vector<bool> &visited, int k){
+        if(visited[i]) return 0;
+        visited[i] = true;
+        long long sum = values[i];
+        for(int j = 0; j < path[i].size(); j++){
+            sum += dfs(path[i][j], path, values, visited, k);
+        }
+        if(sum % k == 0){
+            ans++;
+            return 0;
+        }
+        return sum;
+    }
+
     int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
         vector<vector<int>> path(n);
-        for (int i = 0; i < edges.size(); i++) {
+        for(int i = 0; i < edges.size(); i++){
             path[edges[i][0]].push_back(edges[i][1]);
             path[edges[i][1]].push_back(edges[i][0]);
         }
-
-        vector<bool> visited(n, false);
-        stack<pair<int, long long>> stk; // Pair chứa node và tổng giá trị tính tới node đó
-        vector<long long> subtreeSum(n, 0); // Để lưu giá trị tổng của từng cây con
-        stk.push({0, 0}); // Bắt đầu DFS từ node 0
-        visited[0] = true;
-
-        int ans = 0;
-
-        while (!stk.empty()) {
-            int node = stk.top().first;
-            long long sum = stk.top().second;
-            bool allVisited = true; // Kiểm tra xem tất cả con của node đã được duyệt chưa
-
-            for (int neighbor : path[node]) {
-                if (!visited[neighbor]) {
-                    stk.push({neighbor, 0}); // Đưa con của node vào stack
-                    visited[neighbor] = true;
-                    allVisited = false;
-                }
-            }
-
-            if (allVisited) { // Nếu tất cả con của node đã duyệt
-                stk.pop(); // Lấy node ra khỏi stack
-                long long currentSum = values[node]; // Bắt đầu với giá trị của node
-                for (int neighbor : path[node]) {
-                    if (subtreeSum[neighbor] > 0) { // Thêm tổng của các cây con
-                        currentSum += subtreeSum[neighbor];
-                    }
-                }
-
-                if (currentSum % k == 0) { // Nếu tổng chia hết cho k
-                    ans++;
-                    subtreeSum[node] = 0; // Đặt lại cây con thành 0
-                } else {
-                    subtreeSum[node] = currentSum; // Lưu lại tổng giá trị của cây con
-                }
-            }
-        }
-
+        vector<bool> visited(n);
+        dfs(0, path, values, visited, k);
         return ans;
     }
 };
