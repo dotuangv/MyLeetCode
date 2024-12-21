f62dee869eea2c2c4de1c12adc6614e868b3b93e
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2024-12-21 12:47:49 +0700

    Time: 123 ms (59.49%) | Memory: 173.4 MB (83.54%) - LeetSync

---
+++ 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int ans = 0;
+    int dfs(int i, vector<vector<int>>& path, vector<int>& values, vector<bool> &visited, int k){
+        if(visited[i]){
+            return 0;
+        }
+        visited[i] = true;
+        int sum = values[i] % k;
+        for(int j = 0; j < path[i].size(); j++){
+            sum += dfs(path[i][j], path, values, visited, k);
+            sum %= k;
+        }
+        if(sum % k == 0){
+            ans++;
+            return 0;
+        }
+        return sum;
+    }
+
+    int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
+        vector<vector<int>> path(n);
+        for(int i = 0; i < edges.size(); i++){
+            path[edges[i][0]].push_back(edges[i][1]);
+            path[edges[i][1]].push_back(edges[i][0]);
+        }
+        vector<bool> visited(n);
+        dfs(0, path, values, visited, k);
+        return ans;
+    }
+};
