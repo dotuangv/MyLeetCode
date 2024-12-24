a62b46ec8e6e22c1a8141aff8c6d22189327c401
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2024-12-24 19:36:04 +0700

    Time: 289 ms (59.81%) | Memory: 299.3 MB (67.29%) - LeetSync

--- 3439-find-minimum-diameter-after-merging-two-trees/find-minimum-diameter-after-merging-two-trees.cpp
+++ 3439-find-minimum-diameter-after-merging-two-trees/find-minimum-diameter-after-merging-two-trees.cpp
@@ -27,22 +27,13 @@ public:
             path2[edges2[i][0]].push_back(edges2[i][1]);
             path2[edges2[i][1]].push_back(edges2[i][0]);
         }
-        // bfs(path2);
-        // int res1 = 1e9, res2 = 1e9;
-        // for(auto x: bfs(path1)){
-        //     cout << x << "\n";
-        //     res1 = min(res1, dist(x, path1, visit1));
-        // }
-        // for(auto x: bfs(path2)){
-        //     vector<bool> visit2(m);
-        //     res2 = min(res2, dist(x, path2, visit2));
-        // }
+
         vector<bool> visit1(n), visit2(m);
 
-        int Max1 = 0, Max2 = 0, D1 = dist(0, path1, visit1, Max1), D2 = dist(0, path2, visit2, Max2);
+        int Max1 = 0, Max2 = 0;
+        dist(0, path1, visit1, Max1);
+        dist(0, path2, visit2, Max2);
         return max({Max1, Max2, (Max1 + 1)/2 + (Max2 + 1)/2 + 1});
-        // cout << bfs(path1) << " " << bfs(path2) << "\n";
-        // cout << dist(bfs(path1), path1, visit1) << " " << dist(bfs(path2), path2, visit2) << "\n";
         return 0;
     }
 };
