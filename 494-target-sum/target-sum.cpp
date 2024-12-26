5bead74d9f8ad74fdf8efa6f63791612becd79d5
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2024-12-26 18:46:48 +0700

    Time: 19 ms (54.77%) | Memory: 25.1 MB (29.04%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -2,16 +2,19 @@ class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int> (2001));
-        dp[0][-nums[0] + 1000]++;
-        dp[0][nums[0] + 1000]++;
+        int sum = 0;
+        for(auto x: nums) sum += x;
+        if(sum < abs(target)) return 0;
+        vector<vector<int>> dp(n, vector<int> (2*sum + 1));
+        dp[0][-nums[0] + sum]++;
+        dp[0][nums[0] + sum]++;
         for(int i = 1; i < n; i++){
             int x = nums[i];
-            for(int j = 0; j <= 2000; j++){
+            for(int j = 0; j <= 2*sum; j++){
                 if(j - x >= 0) dp[i][j] += dp[i - 1][j - x];
-                if(j + x <= 2000) dp[i][j] += dp[i - 1][j + x];
+                if(j + x <= 2*sum) dp[i][j] += dp[i - 1][j + x];
             }
         }
-        return dp[n - 1][target + 1000];
+        return dp[n - 1][target + sum];
     }
 };
