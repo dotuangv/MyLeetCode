f5d62d78948bb69d8fd9a8d3e51c36c195eaaca2
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2024-12-28 11:06:59 +0700

    Time: 52 ms (18.16%) | Memory: 46.1 MB (10.30%) - LeetSync

--- 689-maximum-sum-of-3-non-overlapping-subarrays/maximum-sum-of-3-non-overlapping-subarrays.cpp
+++ 689-maximum-sum-of-3-non-overlapping-subarrays/maximum-sum-of-3-non-overlapping-subarrays.cpp
@@ -2,30 +2,38 @@ class Solution {
 public:
     vector<int> maxSumOfThreeSubarrays(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<long long> pre(n + 1);
-        for(int i = 1; i <= n; i++){
+        vector<long long> pre(n + 1, 0);
+
+        // Tính tổng tiền tố
+        for (int i = 1; i <= n; i++) {
             pre[i] = pre[i - 1] + nums[i - 1];
         }
-        vector<vector<pair<long long, int>>> dp(n + 1, vector<pair<long long, int>> (3, {0, -1}));
-        for(int i = 0; i < 3; i++){
-            dp[(i + 1)*k][i] = {pre[(i + 1)*k], i*k + 1};
-            for(int j = (i + 1)*k + 1; j <= n; j++){
-                if(i == 0){
-                    if(pre[j] - pre[j - k] > dp[j - 1][i].first){
-                        dp[j][i] = {pre[j] - pre[j - k], j - k + 1};
-                    }else {
-                        dp[j][i] = dp[j - 1][i];
-                    }
-                }else{
-                    if(pre[j] - pre[j - k] + dp[j - k][i - 1].first > dp[j - 1][i].first){
-                        dp[j][i] = {pre[j] - pre[j - k] + dp[j - k][i - 1].first, j - k + 1};
-                    }else{
-                        dp[j][i] = dp[j - 1][i];
-                    }
+
+        // dp[i][j]: Tổng lớn nhất với j khoảng con và kết thúc tại vị trí i
+        vector<vector<long long>> dp(n + 1, vector<long long>(4, 0));
+        vector<vector<int>> pos(n + 1, vector<int>(4, -1));
+
+        for (int j = 1; j <= 3; j++) {
+            for (int i = j * k; i <= n; i++) {
+                long long sum = pre[i] - pre[i - k];
+                if (dp[i - 1][j] < dp[i - k][j - 1] + sum) {
+                    dp[i][j] = dp[i - k][j - 1] + sum;
+                    pos[i][j] = i - k + 1;
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                    pos[i][j] = pos[i - 1][j];
                 }
             }
         }
-        int c = dp[n][2].second, b = dp[c - 1][1].second, a = dp[b - 1][0].second;
-        return {a - 1, b - 1, c - 1};
+
+        // Truy ngược lại để tìm vị trí
+        vector<int> result(3);
+        int index = n;
+        for (int j = 3; j > 0; j--) {
+            result[j - 1] = pos[index][j] - 1;
+            index = result[j - 1];
+        }
+
+        return result;
     }
 };
