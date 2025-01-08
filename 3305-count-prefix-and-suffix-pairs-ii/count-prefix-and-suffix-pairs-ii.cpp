634bdd0ccfd20a1e3a22793433c23c780e476375
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2025-01-08 10:59:58 +0700

    Time: 669 ms (30.06%) | Memory: 520 MB (19.02%) - LeetSync

--- 3305-count-prefix-and-suffix-pairs-ii/count-prefix-and-suffix-pairs-ii.cpp
+++ 3305-count-prefix-and-suffix-pairs-ii/count-prefix-and-suffix-pairs-ii.cpp
@@ -1,15 +1,7 @@
 class Solution {
 public:
-    struct pair_hash {
-        template <class T1, class T2>
-        size_t operator()(const pair<T1, T2>& p) const {
-            auto hash1 = hash<T1>{}(p.first);
-            auto hash2 = hash<T2>{}(p.second);
-            return hash1 ^ (hash2 << 1); // Kết hợp hai giá trị băm
-        }
-    };
     struct TrieNode {
-        unordered_map<pair<char, char>, TrieNode*, pair_hash> children; // Sử dụng hash cho pair
+        unordered_map<string, TrieNode*> children; 
         int cnt = 0;
     };
 
@@ -24,7 +16,9 @@ public:
         int res = 0, i = 0, j = s.size() - 1;
 
         while (i < s.size()) {
-            pair<char, char> key = {s[i], s[j]};
+            string key = "";
+            key.push_back(s[i]);
+            key.push_back(s[j]);
             if (node->children.find(key) == node->children.end()) {
                 node->children[key] = new TrieNode();
             }
