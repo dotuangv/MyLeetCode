80e23d4b02ef9704b76fe7a7e60f1a9a30a8e078
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2025-01-21 13:14:50 +0700

    Time: 3 ms (59.28%) | Memory: 66.8 MB (61.54%) - LeetSync

--- 2145-grid-game/grid-game.cpp
+++ 2145-grid-game/grid-game.cpp
@@ -2,11 +2,14 @@ class Solution {
 public:
     long long gridGame(vector<vector<int>>& grid) {
         int n = grid[0].size();
-        vector<long long> pre(n + 2), suf(n + 2);
-        for(int i = 1; i <= n; i++) pre[i] = pre[i - 1] + grid[1][i - 1];
-        for(int i = n; i >= 1; i--) suf[i] = suf[i + 1] + grid[0][i - 1];
+        long long top_sum = 0, bot_sum = 0;
         long long ans = 1e18;
-        for(int i = 1; i <= n; i++) ans = min(ans, max(pre[i - 1], suf[i + 1]));
+        for(int i = 0; i < n; i++) top_sum += grid[0][i];
+        for(int i = 0; i < n; i++) {
+            top_sum -= grid[0][i];
+            if(i - 1 >= 0) bot_sum += grid[1][i - 1];
+            ans = min(ans, max(top_sum, bot_sum));
+        }
         return ans;
     }
 };
