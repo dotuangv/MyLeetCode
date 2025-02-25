84b803fda85a48e363dddb2ad7f231409306df6d
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2025-02-25 10:50:39 +0700

    Time: 195 ms (5.23%) | Memory: 186.7 MB (5.77%) - LeetSync

--- 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.cpp
+++ 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.cpp
@@ -5,11 +5,11 @@ public:
         vector<vector<int>> dp(arr.size() + 1, vector<int> (2));
         for(int i = 1; i <= arr.size(); i++){
             if(arr[i - 1]%2){
-                dp[i][0] = dp[i - 1][1]%mod;
-                dp[i][1] = (dp[i - 1][0] + 1)%mod;
+                dp[i][0] = dp[i - 1][1];
+                dp[i][1] = dp[i - 1][0] + 1;
             }else{
-                dp[i][0] = (dp[i - 1][0] + 1)%mod;
-                dp[i][1] = dp[i - 1][1]%mod;
+                dp[i][0] = dp[i - 1][0] + 1;
+                dp[i][1] = dp[i - 1][1];
             }
             ans = (ans + dp[i][1])%mod;
         }
