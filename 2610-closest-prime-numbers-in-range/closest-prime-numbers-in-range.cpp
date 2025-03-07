8ac65c5f99f0822e77a859d1095d11382d37128d
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2025-03-07 21:05:30 +0700

    Time: 20 ms (88.52%) | Memory: 8.9 MB (88.15%) - LeetSync

--- 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.cpp
+++ 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.cpp
@@ -1,27 +1,33 @@
+bool isPrime[1000005] = {false};
+
 class Solution {
 public:
     vector<int> closestPrimes(int left, int right) {
-        vector<bool> prime(right + 1);
-        for(long long i = 2; i <= right; i++){
-            if(!prime[i]){
-                for(long long j = i * i; j <= right; j += i) prime[j] = true;
-            }
+        if (isPrime[2] == 0) {
+            memset(isPrime, 1, sizeof(isPrime));
+            static int arr[2];
+            isPrime[1] = 0;
+
+            for (int i = 2; i <= 1000000; ++i)
+                if (isPrime[i])
+                    for (int j = i << 1; j <= 1000000; j += i)
+                        isPrime[j] = false;
         }
-        vector<int> vt, ans;
-        for(int i = 2; i <= right; i++) if(!prime[i]) vt.push_back(i);
-        int n = vt.size();
-        for(int i = 0; i < n - 1; i++){
-            if(vt[i] >= left && vt[i + 1] <= right){
-                if(ans.empty()){
-                    ans.push_back(vt[i]);
-                    ans.push_back(vt[i + 1]);
-                }else if(vt[i + 1] - vt[i] < ans.back() - ans[0]){
-                    ans[0] = vt[i];
-                    ans[1] = vt[i + 1];
+
+        int idx = 0;
+        vector<int> res{-1, -1};
+
+        for (int i = left, last = -1; i <= right ; ++i)
+            if (isPrime[i]) {
+                if (last != -1) {
+                    if (res[0] == -1)
+                        res[1] = i, res[0] = last;
+                    else if(res[1] - res[0] > i - last)
+                        res[1] = i, res[0] = last;
                 }
+                last = i;
             }
-        }
-        if(ans.empty()) return {-1, -1};
-        return ans;
+        
+        return res;
     }
 };
