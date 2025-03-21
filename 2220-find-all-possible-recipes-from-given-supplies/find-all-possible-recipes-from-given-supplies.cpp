601c5fb522a6741e9b5728c640fdcf471267fce5
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2025-03-21 22:01:16 +0700

    Time: 83 ms (73.49%) | Memory: 109.2 MB (65.78%) - LeetSync

--- 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
@@ -1,31 +1,44 @@
 class Solution {
 public:
-    bool dfs(string x, unordered_map<string, bool> &mp, unordered_map<string, bool> &check, unordered_map<string, vector<string>> &graph){
+    bool dfs(int x, vector<bool> &res, vector<bool> &check, vector<vector<int>> &graph){
         bool ok = true;
         check[x] = true;
         for(auto &y: graph[x]){
-            if(!check[y]) dfs(y, mp, check, graph);
-            ok &= mp[y];
+            if(!check[y]) dfs(y, res, check, graph);
+            ok &= res[y];
         }
         if(graph[x].empty()) ok = false;
-        return mp[x] = ok;
+        return res[x] = ok;
     }
     vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
-        unordered_map<string, bool> mp, check;
-        unordered_map<string, vector<string>> graph;
+        unordered_map<string, int> mp;
+        int cnt = 1;
         for(int i = 0; i < recipes.size(); i++){
+            if(!mp[recipes[i]]) mp[recipes[i]] = cnt++;
             for(auto &x: ingredients[i]){
-                graph[recipes[i]].push_back(x);
+                if(!mp[x]) mp[x] = cnt++;
             }
         }
         for(auto &x: supplies){
-            mp[x] = true;
-            check[x] = true;
+            if(!mp[x]) mp[x] = cnt++;
         }
+        vector<vector<int>> graph(cnt);
+        vector<bool> check(cnt), res(cnt);
+        for(auto &x: supplies){
+            check[mp[x]] = true;
+            res[mp[x]] = true;
+        }
+
+        for(int i = 0; i < recipes.size(); i++){
+            for(auto &x: ingredients[i]){
+                graph[mp[recipes[i]]].push_back(mp[x]);
+            }
+        }
+
         vector<string> ans;
         for(auto &x: recipes){
-            if(!check[x]) dfs(x, mp, check, graph);
-            if(mp[x]) ans.push_back(x);
+            if(!check[mp[x]]) dfs(mp[x], res, check, graph);
+            if(res[mp[x]]) ans.push_back(x);
         }
         return ans;
 
