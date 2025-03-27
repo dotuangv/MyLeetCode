28f29369d2fef51e611ac0b41646edd871f00cc8
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2025-03-27 11:06:00 +0700

    Time: 4 ms (77.46%) | Memory: 89.8 MB (93.85%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
@@ -1,19 +1,39 @@
 class Solution {
+    int mooresVotingAlgo(vector<int>& nums){
+        int majority_element = nums[0];
+        int freq = 1;
+        for(int i=1;i<nums.size();++i){
+            if(nums[i]!=majority_element)   freq--;
+            else                            freq++;
+            
+            if(freq==0){
+                majority_element = nums[i];
+                freq = 1;
+            }
+        }
+        return majority_element;
+    }
 public:
     int minimumIndex(vector<int>& nums) {
-        int n = nums.size();
-        unordered_map<int, int> mp1, mp2;
-        for(int i = 0; i < n; i++){
-            mp1[nums[i]]++;
-        }
-        int x = nums[0];
-        for(int i = 0; i < n - 1; i++){
-            mp2[nums[i]]++;
-            if(mp2[nums[i]] > mp2[x]) x = nums[i];
-            else if(mp2[nums[i]] == mp2[x]){
-                if(mp1[nums[i]] > mp1[x]) x = nums[i];
+        int n=nums.size();
+        //Step-1: Find Majority element
+        int majority_element = mooresVotingAlgo(nums);
+
+        //Step-2: Count frequency of majority_element
+        int max_freq = 0;
+        for(int i=0;i<n;++i)
+            if(nums[i]==majority_element)
+                max_freq++;
+
+        //Step-3: Now find minimum valid split
+        int prefix_count = 0;
+        for(int i=0;i<n-1;++i){
+            if(nums[i]==majority_element){
+                prefix_count++;
+                max_freq--;
             }
-            if(mp2[x] > (i + 1)/2 && (mp1[x] - mp2[x]) > (n - i - 1)/2) return i;
+            if((prefix_count > (i+1)/2) and (max_freq > (n-i-1)/2))
+                return i;
         }
         return -1;
     }
