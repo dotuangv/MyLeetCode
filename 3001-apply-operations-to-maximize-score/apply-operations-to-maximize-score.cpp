7873f156120e6282dfdecd376a1b4f9ef7a04623
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2025-03-29 10:03:44 +0700

    Time: 112 ms (99.39%) | Memory: 138.1 MB (78.66%) - LeetSync

--- 3001-apply-operations-to-maximize-score/apply-operations-to-maximize-score.cpp
+++ 3001-apply-operations-to-maximize-score/apply-operations-to-maximize-score.cpp
@@ -1,148 +1,101 @@
-const int N=sqrt(100000);
-vector<bool> isPrime(N+1, 1);
-vector<int> prime;
-int st[100000];
+#include <bits/stdc++.h>
+using namespace std;
 
-using int2 = pair<int, int>;
+const int MOD = 1e9 + 7;
+const int MAXN = 100000;
+int spf[MAXN + 1];
 
-// radix sort for int2
-const int Bz=256, Bmask=255, bz=8;
-vector<int2> bucket[Bz];
-void radix_sort(vector<int2>& nums, int N) {
-    int xmin=INT_MAX, xmax=INT_MIN;
-    for(int i=0; i<N; i++){
-        auto& [x, _]=nums[i];
-        xmin=min(xmin, x);
-        xmax=max(xmax, x);
-    }
-    unsigned xrange = xmax- xmin + 1;
-    int round = max(1, int(32-countl_zero(xrange) + bz-1) / bz);
-
-    // 0th round
-    for (auto& [x, idx] : nums) {
-        x -= xmin; // Adjust x by xmin
-        bucket[x & Bmask].emplace_back(x += (round == 1) ? xmin : 0, idx);
-    }
-
-    int i = 0;
-    for (auto& B : bucket) {
-        for (auto& v : B)
-            nums[i++] = v;
-        B.clear(); // Clear bucket after usage
-    }
-    //    print(nums);
-
-    // Rounds from 1st to (round-2)th round
-    for (int rd = 1; rd < round - 1; rd++) {
-        int shift = rd * bz; // Bit shifting to extract the next set of bz bits
-        for (auto& [x, idx] : nums)
-            bucket[(x >> shift) & Bmask].emplace_back(x, idx);
-
-        i = 0;
-        for (auto& B : bucket) {
-            for (auto& v : B)
-                nums[i++] = v;
-            B.clear(); // Clear bucket after usage
+auto init = []() {
+    memset(spf, -1, sizeof(spf));
+    spf[0] = spf[1] = 0;
+    for(int i = 2; i <= MAXN; i++) {
+        if(spf[i] == -1) {
+            spf[i] = i;
+            if(i > 317) continue;
+            for(int j = i * i; j <= MAXN; j += i) {
+                if(spf[j] == -1) spf[j] = i;
+            }
         }
-        //   print(nums);
     }
-
-    // Last round
-    if (round > 1) {
-        int shift = bz * (round - 1); // Bit shifting for the last round
-        for (auto& [x, idx] : nums)
-            bucket[x >> shift].emplace_back(x, idx);
-
-        i = 0;
-        for (auto& B : bucket) {
-            for (auto& [x, idx] : B)
-                nums[i++] = {x + xmin, idx}; // Adjust x back by adding xmin
-            B.clear();                       // Clear bucket after usage
+    return 0;
+}();
+void compute(auto& nums, auto& res, const auto& cmp, bool reverse) {
+    int n = nums.size();
+    vector<int> stk;
+    for(int i = reverse ? n-1 : 0, delta = reverse ? -1 : +1; 0 <= i && i < n; i += delta) {
+        while(!stk.empty() && cmp(nums[i], nums[stk.back()])) {
+            res[stk.back()] = i;
+            stk.pop_back();
         }
-        //    print(nums);
+        stk.push_back(i);
     }
+}
+
+int distinct(int n) {
+    if(n <= 1) return 0;
+    int cnt = 0, last = 0;
+    while(n > 1) {
+        if(spf[n] > last) {
+            cnt++, last = spf[n];
+        }
+        n /= spf[n];
+    }
+    return cnt;
+}
+
+long long mod_exp(long long base, int exp, long long mod) {
+    long long ans = 1;
+    base %= mod;
+    while(exp > 0) {
+        if(exp & 1) ans = (ans * base) % mod;
+        exp >>= 1;
+        base = (base * base) % mod;
+    }
+    return ans;
 }
 
 class Solution {
 public:
-    const static int mod = 1e9 + 7;
-    // Eratosthenes' Sieve
-    constexpr static void Sieve() {
-        if (!prime.empty()) return;
-        isPrime[0] = isPrime[1] = 0;
-        for (int i = 2; i * i <= N; i++) {
-            if (isPrime[i]) {
-                prime.push_back(i);
-                for (int j = i * i; j <= N; j += i)
-                    isPrime[j] = 0;
-            }
+    int maximumScore(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> factors(n), nextGreater(n, n), prevGreater(n, -1);
+
+        for(int i = 0; i < n; i++) {
+            factors[i] = distinct(nums[i]);            
         }
-        for (int i = 2; i <= N; i++) {
-            if (isPrime[i]) prime.push_back(i);
+        compute(factors, nextGreater, [](int a, int b) { return a > b; }, false);
+        compute(factors, prevGreater, [](int a, int b) { return a >= b; }, true);
+
+        priority_queue<array<int, 2>> pq;
+        for(int i = 0; i < n; i++) {
+            int left = prevGreater[i] + 1, right = nextGreater[i] - 1;
+
+            // sum from x = (left) -> (i) : right - x + 1
+            // sum from x = (left) -> (i) : (right + 1) - x
+            // (i - left + 1) * (right + 1) - sum from x = (left) -> (i) : x
+            // (i - left + 1) * (right + 1) - sum from x = (left) -> (i) : x
+            // (i - left + 1) * (right + 1) - (left + (left + 1) + ... + i)
+            // (i - left + 1) * (right + 1) - (i - left + 1) * (left + i) / 2
+            // (i - left + 1) * (right + 1 - (left + i) / 2)
+            // (i - left + 1) * ([2right + 2 - (left + i)] / 2)
+            // (i - left + 1) * ([2right + 2 - left - i] / 2)
+            // (i - left + 1) * (2right - left - i + 2) / 2
+            int ch_left = i - left + 1;
+            int ch_right = right - i + 1;
+            long long cnt = 1LL * ch_right * ch_left;
+            // cout << "i = " << i << ", nums[i] = " << nums[i] << ", factors[i] = " << factors[i] << ", cnt = " << cnt << '\n';
+            // cout << "Range [" << left << ", " << right << "]\n";
+            pq.push({nums[i], (int) min(cnt, (long long) 1e9)});
         }
-    }
-
-    //computing prime score
-    static int prime_score(int x){
-        if (x<=N && isPrime[x]) return 1;
-        int xsqrt=sqrt(x);
-        int cnt=0;
-        for(int p: prime){
-            if (p>xsqrt) break;
-            if (x%p!=0) continue;
-            while (x%p==0){
-                x/=p;
-            }
-            cnt++;
-        }
-        cnt+=(x>1);
-        return cnt;
-    }
-
-    // Modular exponentiation
-    inline static long long modPow(long long x, int exp) {
-        long long ans= 1;
-        while (exp>0) {
-            if (exp&1) ans=ans*x % mod;
-            x=x*x%mod;
-            exp/=2;
-        }
-        return ans;
-    }
-
-    static int maximumScore(vector<int>& nums, int k) {
-        const int n = nums.size();
-        Sieve();
-
-        vector<int> score(n), left(n), right(n);
-        for (int i=0; i< n; i++) score[i] = prime_score(nums[i]);
-
-        int top=-1;
-        for (int i=0; i < n; i++) {
-            while ( top!=-1 && score[i] > score[st[top]]) top--;
-            left[i] = top==-1 ? -1 : st[top];
-            st[++top]=i;
-        }
-
-        top=-1;
-        for (int i = n - 1; i >= 0; i--) {
-            while (top!=-1 && score[i] >= score[st[top]]) top--;
-            right[i] = top==-1 ? n : st[top];
-            st[++top]=i;
-        }
-
-        vector<int2> num_idx(n);
-        for (int i = 0; i < n; i++)
-            num_idx[i] = {nums[i], i};
-
-        radix_sort(num_idx, n);
-
-        long long ans=1;
-        for(int i=n-1; i>=0 && k>0; i--){// inverse transversal
-            auto [x, idx]=num_idx[i];
-            int exp=min((long long)(idx-left[idx])*(right[idx]-idx), (long long)k);
-            ans=(ans*modPow(x, exp)%mod);
-            k-=exp;
+        long long ans = 1;
+        while(!pq.empty() && k > 0) {
+            auto [mult, cnt] = pq.top();
+            pq.pop();
+            int take = min(k, cnt);
+            // cout << "Take " << take << "x of score = " << mult << '\n';
+            // cout << "MEXP: " << mod_exp(mult, take, MOD) << '\n';
+            ans = (ans * mod_exp(mult, take, MOD)) % MOD;
+            k -= take;
         }
         return ans;
     }
