f7e1a50407d4be7ddc82a64ac0a7424ecbd06d7e
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2025-04-08 13:33:20 +0700

    Time: 0 ms (100.00%) | Memory: 26.8 MB (99.66%) - LeetSync

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
-        int cnt = 0, temp;
-        while (true) {
-            unordered_map<int, int> mpp;
-            temp = 0;
-            for (int num : nums)
-                if (++mpp[num] == 2) temp++;
-
-            if (temp == 0) break;
-            nums.erase(nums.begin(), nums.begin() + min(3, (int)nums.size()));
-            cnt++;
+        vector<bool> seen(128);
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            if (seen[nums[i]]) {
+                return i / 3 + 1;
+            }
+            seen[nums[i]] = true;
         }
-        return cnt;
+        return 0;
     }
 };
