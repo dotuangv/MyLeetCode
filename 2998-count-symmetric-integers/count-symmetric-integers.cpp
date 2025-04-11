a6b801748e36164250c3f907ed2701f72f105b40
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2025-04-11 16:19:15 +0700

    Time: 539 ms (10.27%) | Memory: 130.2 MB (9.15%) - LeetSync

--- 2998-count-symmetric-integers/count-symmetric-integers.cpp
+++ 2998-count-symmetric-integers/count-symmetric-integers.cpp
@@ -3,19 +3,20 @@ public:
     int countSymmetricIntegers(int low, int high) {
         int ans = 0;
         for(int i = low; i <= high; i++){
-            vector<int> v;
-            int x = i;
-            while(x){
-                v.push_back(x % 10);
-                x /= 10;
+            if((int)log10(i) % 2){
+                vector<int> v;
+                int x = i;
+                while(x){
+                    v.push_back(x % 10);
+                    x /= 10;
+                }
+                int s1 = 0, s2 = 0;
+                for(int j = 0; j < v.size(); j++){
+                    if(j < v.size() / 2) s1 += v[j];
+                    else s2 += v[j];
+                }
+                if(s1 == s2) ans++;
             }
-            if(v.size() % 2) continue;
-            int s1 = 0, s2 = 0;
-            for(int j = 0; j < v.size(); j++){
-                if(j < v.size() / 2) s1 += v[j];
-                else s2 += v[j];
-            }
-            if(s1 == s2) ans++;
         }
         return ans;
     }
