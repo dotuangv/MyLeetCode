62b08b07ac67027453f5c2bec867c68936883def
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2025-04-21 11:28:39 +0700

    Time: 3 ms (37.10%) | Memory: 112.6 MB (75.00%) - LeetSync

--- 2249-count-the-hidden-sequences/count-the-hidden-sequences.cpp
+++ 2249-count-the-hidden-sequences/count-the-hidden-sequences.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     int numberOfArrays(vector<int>& differences, int lower, int upper) {
         int n = differences.size();
-        vector<long long> Lower(n + 1), Upper(n + 1);
-        Lower[0] = lower; Upper[0] = upper;
+        long long Lower = lower, Upper = upper;
         for(int i = 1; i <= n; i++){
-            Lower[i] = max((long long)lower, Lower[i - 1] + differences[i - 1]);
-            Upper[i] = min((long long)upper, Upper[i - 1] + differences[i - 1]);
+            Lower = max((long long)lower, Lower + differences[i - 1]);
+            Upper = min((long long)upper, Upper + differences[i - 1]);
         }
-        return max(0LL, Upper[n] - Lower[n] + 1);
+        return max(0LL, Upper - Lower + 1);
     }
 };
