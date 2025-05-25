3bbca1a6b4b789478ef2bc57d9efc02fa645836c
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2025-05-25 11:47:38 +0700

    Time: 11 ms (95.68%) | Memory: 172.3 MB (14.61%) - LeetSync

--- 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -2,25 +2,23 @@ class Solution {
 public:
     int longestPalindrome(vector<string>& words) {
         int ans = 0, res = 0;
-        unordered_map<string, int> mp;
-        for(auto &s: words) mp[s]++;
+        vector<vector<int>> mp(26, vector<int> (26));
+        for(auto &s: words) mp[s[0] - 'a'][s[1] - 'a']++;
         for(auto &s: words) {
             if(s[0] == s[1]){
-                if(mp[s] >= 2){
+                if(mp[s[0] - 'a'][s[1] - 'a'] >= 2){
                     ans += 4;
-                    mp[s] -= 2;
-                }else if(mp[s] == 1){
-                    mp[s]--;
+                    mp[s[0] - 'a'][s[1] - 'a'] -= 2;
+                }else if(mp[s[0] - 'a'][s[1] - 'a'] == 1){
+                    mp[s[0] - 'a'][s[1] - 'a']--;
                     res = 2;
                 }
             }
             else {
-                string s2 = s;
-                swap(s[0], s[1]);
-                if(mp[s] && mp[s2]){
+                if(mp[s[0] - 'a'][s[1] - 'a'] && mp[s[1] - 'a'][s[0] - 'a']){
                     ans += 4;
-                    mp[s]--;
-                    mp[s2]--;
+                    mp[s[0] - 'a'][s[1] - 'a']--;
+                    mp[s[1] - 'a'][s[0] - 'a']--;
                 }
             }
         }
