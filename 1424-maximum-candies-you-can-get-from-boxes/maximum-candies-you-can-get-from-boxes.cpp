a8252682a21fbb9559da9bb7ada3e44e249b9091
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2025-06-04 00:50:15 +0700

    Time: 3 ms (68.75%) | Memory: 44.3 MB (62.50%) - LeetSync

--- 1424-maximum-candies-you-can-get-from-boxes/maximum-candies-you-can-get-from-boxes.cpp
+++ 1424-maximum-candies-you-can-get-from-boxes/maximum-candies-you-can-get-from-boxes.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
-    int maxCandies(vector<int>& vis, vector<int>& candies, vector<vector<int>>& keys, vector<vector<int>>& containedBoxes, vector<int>& initialBoxes) {
-        bool foundOpenable = true;
-        int totalCandies = 0;
-        while (!initialBoxes.empty() && foundOpenable) {
-            foundOpenable = false;
-            vector<int> nextBoxes;
-            for (int boxId : initialBoxes) {
-                if (vis[boxId]) {
-                    foundOpenable = true;
-                    nextBoxes.insert(end(nextBoxes), begin(containedBoxes[boxId]), end(containedBoxes[boxId]));
-                    for (int keyId : keys[boxId]) vis[keyId] = 1;
-                    totalCandies += candies[boxId];
-                } else {
-                    nextBoxes.push_back(boxId);
+    int maxCandies(vector<int>& status, vector<int>& candies, vector<vector<int>>& keys, vector<vector<int>>& containedBoxes, vector<int>& initialBoxes) {
+        int cnt=0;
+        queue<int> q;
+        for(auto it:initialBoxes){ q.push(it); }
+        while(!q.empty()){
+            int sz=q.size();
+            vector<int> curr; vector<int> inserted;
+            while(sz--){
+                int box=q.front(); q.pop();
+                curr.push_back(box);
+                if(status[box]){
+                    cnt+=candies[box]; 
+                    for(auto key:keys[box]){status[key]=1;}
+                    for(auto other:containedBoxes[box]){q.push(other);}
+                }
+                else{
+                    q.push(box);
+                    inserted.push_back(box);
                 }
             }
-            swap(initialBoxes, nextBoxes);
+            if(curr==inserted){
+                break;
+            }
         }
-        return totalCandies;
+        return cnt;
     }
 };
