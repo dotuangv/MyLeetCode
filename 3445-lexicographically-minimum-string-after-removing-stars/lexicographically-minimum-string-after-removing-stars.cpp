266f06ca55554fb14ae25433810d5c46a06d9f78
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2025-06-08 00:43:24 +0700

    Time: 180 ms (27.92%) | Memory: 26.5 MB (60.06%) - LeetSync

---
+++ 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string clearStars(string s) {
+        priority_queue<pair<char, int>, vector<pair<char, int>>, greater<pair<char, int>>> pq;
+
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] != '*') pq.push({s[i], -i});
+            else if(!pq.empty()) pq.pop();
+        }
+
+        vector<bool> mp(s.size());
+        while(!pq.empty()){
+            mp[-pq.top().second] = true;
+            pq.pop();
+        }
+
+        string ans = "";
+        for(int i = 0; i < s.size(); i++){
+            if(mp[i]) ans.push_back(s[i]);
+        }
+        return ans;
+    }
+};
