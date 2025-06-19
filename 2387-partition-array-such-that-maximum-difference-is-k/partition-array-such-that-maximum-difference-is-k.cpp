6688ad3c75ac443c65abeb2d254c0544c09b17d3
Đỗ Văn Tuấn <117527123+dotuangv@users.noreply.github.com>
2025-06-20 00:02:59 +0700

    Time: 8 ms (96.70%) | Memory: 88 MB (6.80%) - LeetSync

--- 2387-partition-array-such-that-maximum-difference-is-k/partition-array-such-that-maximum-difference-is-k.cpp
+++ 2387-partition-array-such-that-maximum-difference-is-k/partition-array-such-that-maximum-difference-is-k.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
     int partitionArray(vector<int>& nums, int k) {
-        ranges::sort(nums);
-        int ans = 1;
-        int rec = nums[0];
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] - rec > k) {
-                ans++;
-                rec = nums[i];
+        std::bitset<100001> exists;
+        int minV = nums[0];
+        int maxV = nums[0];
+
+        for (auto v: nums) {
+            minV = std::min(minV, v);
+            maxV = std::max(maxV, v);
+            exists[v] = true;
+        }
+
+        int seq = 1;
+        int start = minV;
+        for (int v = minV; v <= maxV; ++v) {
+            if (exists[v]) {
+                if (v - start > k) {
+                    start = v;
+                    ++seq;
+                }
             }
         }
-        return ans;
+
+        return seq;
     }
 };
